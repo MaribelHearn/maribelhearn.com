3f4399b63c8c4a6faedede5449e942563167acfb
Maribel Hearn <casbas.sj@gmail.com>
2017-09-09 14:30:52 +0200

    Update drc.js

--- drc.js
+++ drc.js
@@ -1126,6 +1126,7 @@ function checkValues(changePerformance, changeShottypes, doubleSpoilerCheck) {
     
     if (changePerformance) {
         $(ROUTE).css("display", "none");
+        $(SEASON).css("display", "none");
         
         if (changeShottypes) {
             if (game == "PCB") {
