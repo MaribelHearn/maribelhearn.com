000264e4a897484828f44f3235267545046ecc04
Maribel Hearn <maribelhearn@disroot.org>
2017-09-29 22:08:35 +0200

    Use JS for back and notice

---
+++ thvote.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    // detect smartphone and tablet
+    if (navigator.userAgent.indexOf("Mobile") > -1 || navigator.userAgent.indexOf("Tablet") > -1) {
+        $("#notice").css("display", "block");
+        $("#back").css("display", "block");
+	}
+});
