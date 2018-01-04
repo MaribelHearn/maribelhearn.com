545782c7cb8fa57d1fae2e85e4b0c7f78ddbf40e
Maribel Hearn <casbas.sj@gmail.com>
2018-01-04 16:02:23 +0100

    Fix autosort

--- lnn/lnn.js
+++ lnn/lnn.js
@@ -137,6 +137,7 @@ $(document).ready(function() {
         }
         
         $("#autosort2").click();
+        $("#autosort2").click();
         $("#autosort1").click();
         $("#autosort1").click();
     }, "json");
