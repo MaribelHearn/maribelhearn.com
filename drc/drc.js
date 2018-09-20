acf82abef57c406f76266eeb03086dd2887743ec
Maribel Hearn <casbas.sj@gmail.com>
2018-09-20 16:48:40 +0200

    [DRC] Countdown to start

--- drc/drc.js
+++ drc/drc.js
@@ -1201,14 +1201,14 @@ $(document).ready(function() {
 function updateCountdown() {
     var countdownDate, now, distance, days, hours, minutes, seconds;
     
-    countdownDate = Date.UTC("2018", "6", "9", "2", "30", "0");
+    countdownDate = Date.UTC("2018", "9", "3", "17", "0", "0");
     now = new Date().getTime();
     distance = countdownDate - now;
     days = Math.floor(distance / (1000 * 60 * 60 * 24));
     hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    $(COUNTDOWN).html("DRC End<br>" + days + translate("d ") + hours + translate("h ") + minutes + translate("m ") + seconds + translate("s"));
+    $(COUNTDOWN).html("DRC Start<br>" + days + translate("d ") + hours + translate("h ") + minutes + translate("m ") + seconds + translate("s"));
     
     if (distance < 0) {
         $(COUNTDOWN).html("");
