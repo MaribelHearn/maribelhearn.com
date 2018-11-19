ca9048e8675b3d198df2adadf1b295df8f214ba5
MaribelHearn <casbas.sj@gmail.com>
2018-11-20 00:17:07 +0100

    [Thvote] Mobile notice

--- thvote/thvote.js
+++ thvote/thvote.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
+var ready = function () {
     // detect smartphone and tablet
     if (navigator.userAgent.indexOf("Mobile") > -1 || navigator.userAgent.indexOf("Tablet") > -1) {
         document.getElementById("notice").style.display = "block";
 	}
-});
+};
