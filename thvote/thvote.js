e9393be10628280075e429a961cc5a15baa53240
Maribel Hearn <casbas.sj@gmail.com>
2018-11-19 22:42:48 +0100

    [DRC] Do not translate 'WR'

--- thvote/thvote.js
+++ thvote/thvote.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     // detect smartphone and tablet
     if (navigator.userAgent.indexOf("Mobile") > -1 || navigator.userAgent.indexOf("Tablet") > -1) {
-        $("#notice").css("display", "block");
+        document.getElementById("notice").style.display = "block";
 	}
 });
