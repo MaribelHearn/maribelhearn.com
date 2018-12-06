cf04568b7a194e230c2d80f0bdf5aa0e6a18f031
MaribelHearn <casbas.sj@gmail.com>
2018-12-06 19:47:09 +0100

    [Scoring] Update cookie system

--- scoring/scoring.js
+++ scoring/scoring.js
@@ -1,4 +1,7 @@
-﻿var WRs, NUMBER_OF_GAMES = 16, tracked = ["EoSD", "PCB", "IN", "MoF", "SA", "UFO", "GFW", "TD", "DDC", "LoLK", "HSiFS"], untracked = ["HRtP", "SoEW", "PoDD", "LLS", "MS", "PoFV"], scores = {
+﻿var WRs,
+tracked = ["EoSD", "PCB", "IN", "MoF", "SA", "UFO", "GFW", "TD", "DDC", "LoLK", "HSiFS"],
+untracked = ["HRtP", "SoEW", "PoDD", "LLS", "MS", "PoFV"],
+scores = {
   "HRtP": {
     "Easy": {
       "Makai": 0,
@@ -740,17 +743,17 @@
   }
 };
 
-function show(game, skip) {
+function show(game) {
     $("#" + game).css("display", "block");
-    
+
     if (!$("#" + game + "c").is(":checked")) {
         $("#" + game + "c").prop("checked", true);
     }
 }
 
-function hide(game, skip) {
+function hide(game) {
     $("#" + game).css("display", "none");
-    
+
     if ($("#" + game + "c").is(":checked")) {
         $("#" + game + "c").prop("checked", false);
     }
@@ -758,183 +761,212 @@ function hide(game, skip) {
 
 function checkGame(arg) {
     if ($("#" + arg + "c").is(":checked")) {
-        show(arg, true);
+        show(arg);
     } else {
-        hide(arg, true);
+        hide(arg);
     }
 }
 
 function checkTracked() {
     var checked = $("#tracked").is(":checked");
-    
+
     for (var key in tracked) {
         if (checked) {
-            show(tracked[key], false);
+            show(tracked[key]);
         } else {
-            hide(tracked[key], false);
+            hide(tracked[key]);
         }
     }
 }
 
 function checkUntracked() {
     var checked = $("#untracked").is(":checked");
-    
+
     for (var key in untracked) {
         if (checked) {
-            show(untracked[key], false);
+            show(untracked[key]);
         } else {
-            hide(untracked[key], false);
+            hide(untracked[key]);
         }
     }
 }
 
 function checkAll() {
-    var checked = $("#all").is(":checked");
-    
-    if (checked) {
-        $("#tracked").prop("checked", true);
-        $("#untracked").prop("checked", true);
-    } else {
-        $("#tracked").prop("checked", false);
-        $("#untracked").prop("checked", false);
-    }
-    
-    for (var key in scores) {
-        if (checked) {
-            show(scores[key], false);
-        } else {
-            hide(scores[key], false);
-        }
-    }
+    var checked = $("#all").is(":checked"), key;
+
+    $("#tracked").prop("checked", checked);
+    $("#untracked").prop("checked", checked);
+    checkTracked();
+    checkUntracked();
 }
 
 var calc = function () {
-    var top = {}, averages = {}, total = 0, categories = 0, highest = 0,
+    var top = {}, averages = {}, shown = {}, total = 0, categories = 0, highest = 0,
         game, difficulty, id, span, score, shottype, wr, percentage, wrText, average, table, gameTable,
         topList = "<table id='table'><thead><tr><th>Game + Difficulty</th><th>Shottype / Route</th><th class='sorttable_numeric'>Score</th><th>WR Percentage</th><th>Progress Bar</th><th>WR</th></tr></thead><tbody>",
         precision = parseInt($("#precision").val());
-        
+
     if (isNaN(precision) || precision < 0 || precision > 5) {
         $("#error").html("<b style='color:red'>Invalid precision; minimum is 0, maximum is 5.</b>");
         return;
     } else {
         $("#error").html("");
     }
-    
+
     for (game in WRs) {
         if ($("#" + game).css("display") == "none") {
             continue;
         }
-        
+
         total = 0;
         categories = 0;
-        
+
         for (difficulty in WRs[game]) {
             for (shottype in WRs[game][difficulty]) {
                 id = "#" + game + difficulty + shottype;
                 score = $(id).val().replace(/,/g, "").replace(/\./g, "").replace(/ /g, "");
-                
+
                 if (score === "") {
                     $("#error").html("");
                     continue;
                 }
-                
+
                 if (isNaN(score)) {
                     $("#error").html("<b style='color:red'>You entered one or more invalid scores. Please use only digits, dots, commas and spaces.</b>");
                     return;
                 }
-                
+
                 score = parseInt(score);
                 wr = WRs[game][difficulty][shottype];
                 scores[game][difficulty][shottype] = score;
-                
+
                 if (score == wr[0]) {
                     hack = true;
                     score -= 1;
                 }
-                
+
                 if (wr[0] === 0) {
                     percentage = '-';
                     wrText = '-';
                 } else {
                     percentage = score / wr[0] * 100;
                     wrText = sep(wr[0]) + " by <i>" + wr[1] + "</i>";
-                    
+
                     if (percentage > highest) {
                         highest = percentage;
                     }
-                    
+
                     percentage = (precision === 0 ? Math.round(percentage) : Number(percentage).toFixed(precision));
                     total += Number(percentage);
                     categories++;
                 }
-                
+
                 topList += "<tr><td>" + game + " " + difficulty + "</td><td>" + shottype.replace("Team", " Team") + "</td><td>" + sep(score) + "</td><td>" + percentage +
                 "%</td><td><progress value='" + percentage + "' max='100'></progress></td><td>" + wrText + "</td>";
             }
         }
-        
+
         if (categories > 0) {
             average = total / categories;
             averages[game] = (precision === 0 ? Math.round(average) : Number(average).toFixed(precision));
         }
     }
-    
+
     topList += "</tbody></table><br><table id='gameTable'><thead><tr><th>Game</th><th>Average Percentage</th></tr></thead><tbody>";
-    
+
     for (game in averages) {
         topList += "<tr><td>" + game + "</td><td>" + averages[game] + "%</td></tr>";
     }
-    
+
     topList += "</tbody></table>";
-    
+
     if (highest === 0) {
         $("#error").html("<b style='color:red'>You have no significant scores! Try to score some more!</b>");
         $("#topList").html("");
         return;
     }
-    
+
     $("#topList").html(topList);
     sorttable.makeSortable(document.getElementById("table"));
     sorttable.makeSortable(document.getElementById("gameTable"));
-    
+
+    // save cookies
     if ($("#toggleCookie").is(":checked")) {
+        setCookie("precision", precision);
+
         for (game in scores) {
+            shown[game] = $("#" + game + "c").is(":checked");
+            setCookie("shown", shown);
             setCookie(game, scores[game]);
         }
     }
 };
 
-$(document).ready(function() {
+var reset = function () {
+    var confirmation = confirm("Are you sure you want to erase all your scores?"),
+        game, difficulty, shottype;
+
+    if (confirmation) {
+        deleteCookie("shown");
+        deleteCookie("precision");
+
+        for (game in scores) {
+            deleteCookie(game);
+
+            for (difficulty in scores[game]) {
+                for (shottype in scores[game][difficulty]) {
+                    scores[game][difficulty][shottype] = 0;
+                    $("#" + game + difficulty + shottype).val("");
+                }
+            }
+        }
+    }
+};
+
+var loadGameCookies = function () {
     var game, cookie, difficulty, shottype;
-    
+
     for (game in scores) {
-        try {
-            cookie = getCookie(game);
-            
-            if (cookie) {
-                scores[game] = cookie;
-                
-                for (difficulty in scores[game]) {
-                    for (var shottype in scores[game][difficulty]) {
-                        if (scores[game][difficulty][shottype] !== 0) {
-                            $("#" + game + difficulty + shottype).val(sep(scores[game][difficulty][shottype]));
-                        }
+        cookie = getCookie(game);
+
+        if (cookie) {
+            scores[game] = cookie;
+
+            for (difficulty in scores[game]) {
+                for (var shottype in scores[game][difficulty]) {
+                    if (scores[game][difficulty][shottype] !== 0) {
+                        $("#" + game + difficulty + shottype).val(sep(scores[game][difficulty][shottype]));
                     }
                 }
             }
-        } catch (err) {
-            // do nothing
         }
-        
     }
-    
-    // detect smartphone and tablet
+};
+
+var loadShownCookie = function () {
+    var shownCookie = getCookie("shown"), game;
+
+    for (game in shownCookie) {
+        if (!shownCookie[game]) {
+            hide(game);
+        }
+    }
+};
+
+$(document).ready(function() {
+    try {
+        loadGameCookies();
+        loadShownCookie();
+        $("#precision").val(getCookie("precision") === "" ? 0 : getCookie("precision"));
+    } catch (err) {
+        // do nothing
+    }
+
     if (navigator.userAgent.contains("Mobile") || navigator.userAgent.contains("Tablet")) {
         $("#notice").css("display", "block");
 	}
-    
-    $.get("../json/wrlist.json", function(data) {
+
+    $.get("https://maribelhearn.github.io/json/wrlist.json", function (data) {
         WRs = data;
     }, "json");
 });
