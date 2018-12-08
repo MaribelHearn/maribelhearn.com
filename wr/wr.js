dda88b46c7cf955dfd088b97f9d40f5fc2d647ff
Maribel Hearn <maribelhearn@disroot.org>
2018-12-08 15:25:35 +0100

    [WR] Absolute JSON paths

--- wr/wr.js
+++ wr/wr.js
@@ -3,34 +3,34 @@ var WRs, playerWRs, compareWRs, westScores, skips = [], all = ["overall", "HRtP"
 
 function bestSeason(difficulty, shottype) {
     var shottypes = WRs.HSiFS[difficulty], max = 0, season, i;
-    
+
     for (i in shottypes) {
         if (!i.contains(shottype)) {
             continue;
         }
-        
+
         if (shottypes[i][0] > max) {
             season = i.replace(shottype, "");
             max = shottypes[i][0];
         }
     }
-    
+
     return season;
 }
 
 function show(game, skip) {
     $("#" + game).css("display", "block");
     $("#" + game + "o").css("display", "table-row");
-    
+
     for (var difficulty in WRs[game]) {
         $("#" + game + difficulty).css("display", "table-row");
         $("#cat_" + game + difficulty).css("display", "table-row");
     }
-    
+
     if (!$("#" + game + "c").is(":checked")) {
         $("#" + game + "c").prop("checked", true);
     }
-    
+
     if (skip && skips.contains(game)) {
         skips.remove(game);
         load();
@@ -40,16 +40,16 @@ function show(game, skip) {
 function hide(game, skip) {
     $("#" + game).css("display", "none");
     $("#" + game + "o").css("display", "none");
-    
+
     for (var difficulty in WRs[game]) {
         $("#" + game + difficulty).css("display", "none");
         $("#cat_" + game + difficulty).css("display", "none");
     }
-    
+
     if ($("#" + game + "c").is(":checked")) {
         $("#" + game + "c").prop("checked", false);
     }
-    
+
     if (skip && !skips.contains(game)) {
         skips.pushStrict(game);
         load();
@@ -66,7 +66,7 @@ function checkGame(arg) {
 
 function checkTracked() {
     var checked = $("#tracked").is(":checked");
-    
+
     for (var key in tracked) {
         if (checked) {
             show(tracked[key], false);
@@ -76,13 +76,13 @@ function checkTracked() {
             skips.pushStrict(tracked[key]);
         }
     }
-    
+
     load();
 }
 
 function checkUntracked() {
     var checked = $("#untracked").is(":checked");
-    
+
     for (var key in untracked) {
         if (checked) {
             show(untracked[key], false);
@@ -92,13 +92,13 @@ function checkUntracked() {
             skips.pushStrict(untracked[key]);
         }
     }
-    
+
     load();
 }
 
 function checkAll() {
     var checked = $("#all").is(":checked");
-    
+
     if (checked) {
         $("#tracked").prop("checked", true);
         $("#untracked").prop("checked", true);
@@ -106,7 +106,7 @@ function checkAll() {
         $("#tracked").prop("checked", false);
         $("#untracked").prop("checked", false);
     }
-    
+
     for (var key in all) {
         if (checked) {
             show(all[key], false);
@@ -116,7 +116,7 @@ function checkAll() {
             skips.pushStrict(all[key]);
         }
     }
-    
+
     load();
 }
 
@@ -135,35 +135,35 @@ function percentageClass(percentage) {
 }
 
 function load() {
-    $.get("../json/wrlist.json", function (data) {
+    $.get("https://maribelhearn.github.io/json/wrlist.json", function (data) {
         WRs = data;
         playerWRs = {};
         compareWRs = {};
-        
+
         var skip = {}, game, max, difficulty, bestshotmax, shottypes, shottype, wr, score, player, replay, overall, overallplayer,
         overalldifficulty, overallshottype, overallseason, bestshot, bestshotplayer, bestshotseason, text, count;
-        
+
         for (game in WRs) {
             if (!$("#" + game + "c").is(":checked") || skips.contains(game)) {
                 skips.pushStrict(game);
                 hide(game);
                 continue;
             }
-            
+
             max = 0;
             compareWRs[game] = {};
-            
+
             for (difficulty in WRs[game]) {
                 bestshotmax = 0;
                 shottypes = (game == "HSiFS" ? {"Reimu": "", "Cirno": "", "Aya": "", "Marisa": ""} : WRs[game][difficulty]);
-                
+
                 for (shottype in shottypes) {
                     season = (game == "HSiFS" ? bestSeason(difficulty, shottype) : "");
                     wr = WRs[game][difficulty][shottype + season];
                     score = wr[0];
                     player = wr[1];
                     replay = wr[2];
-                    
+
                     if (score > max) {
                         overall = "#" + game + difficulty + shottype;
                         overallplayer = player;
@@ -172,7 +172,7 @@ function load() {
                         overallseason = season;
                         max = score;
                     }
-                    
+
                     if (score > bestshotmax) {
                         bestshot = "#" + game + difficulty + shottype;
                         bestshotplayer = player;
@@ -180,83 +180,83 @@ function load() {
                         bestshotmax = score;
                         bestshotreplay = replay;
                     }
-                    
+
                     if (player !== "") {
                         if (!playerWRs.hasOwnProperty(player)) {
                             playerWRs[player] = {};
                         }
-                            
+
                         if (!playerWRs[player].hasOwnProperty(game)) {
                             playerWRs[player][game] = 0;
                         }
-                        
+
                         playerWRs[player][game] += 1;
                     }
-                    
+
                     text = (replay === "" ? sep(score) : "<a class='replay' href='" + replay + "'>" + sep(score) + "</a>") + "<br>by <em>" + player + "</em>";
                     score > 0 ? $("#" + game + difficulty + shottype).html(text + (game == "HSiFS" && difficulty != "Extra" ? " (" + season + ")" : "")) : $("#" + game + difficulty + shottype).html('-');
                 }
-                
+
                 $(bestshot).html((bestshotreplay === "" ? "<u>" + sep(bestshotmax) + "</u>" : "<u><a class='replay' href='" + bestshotreplay +
                 "'>" + sep(bestshotmax) + "</a>") + "</u><br>by <em>" + bestshotplayer + "</em>" + (game == "HSiFS" && difficulty != "Extra" ? " (" + bestshotseason + ")" : ""));
                 compareWRs[game][difficulty] = [bestshotmax, bestshotplayer, bestshot.replace("#" + game + difficulty, "") + (game == "HSiFS" && difficulty != "Extra" ? bestshotseason : "")];
             }
-            
+
             $(overall).html($(overall).html().replace("<u>", "<u><strong>").replace("</u>", "</strong></u>"));
-            
+
             // Nanashi suspicion footnote
             if (game == "MoF") {
                 $(overall).html($(overall).html().replace("</strong>", "*</strong>"));
             }
-            
+
             $("#" + game + "overall0").html(sep(max));
             $("#" + game + "overall1").html(overallplayer);
             $("#" + game + "overall2").html(overalldifficulty);
             $("#" + game + "overall3").html(overallshottype + (game == "HSiFS" ? " (" + overallseason + ")" : ""));
         }
-        
+
         $("#ranking_tbody").html("");
         $("#west_tbody").html("");
-        
+
         for (player in playerWRs) {
             count = 0;
-            
+
             for (game in playerWRs[player]) {
                 if (skips.contains(game)) {
                     if (!skip.hasOwnProperty(player)) {
                         skip[player] = 0;
                     }
-                    
+
                     skip[player] += 1;
                     continue;
                 }
-                
+
                 count += playerWRs[player][game];
             }
-            
+
             $("#ranking_tbody").append("<tr id='player_" + player + "'><td>" + player + "</td><td id='player_" + player + "n'>" + count +
             "</td><td id='player_" + player + "g'>" + Object.keys(playerWRs[player]).length + "</td></tr>");
         }
-        
+
         // west scores
         var west, world, percentage;
-        
-        $.get("../json/bestinthewest.json", function (data) {
+
+        $.get("https://maribelhearn.github.io/json/bestinthewest.json", function (data) {
             westScores = data;
-            
+
             for (game in westScores) {
                 if (skips.contains(game)) {
                     continue;
                 }
-                
+
                 $("#west_tbody").append("<tr><td colspan='3'><u>" + game + "</u></td></tr>");
-                
+
                 for (difficulty in westScores[game]) {
                     if (westScores[game][difficulty].length === 0) {
                         $("#" + game + difficulty).append("<td>-</td><th>-</th>");
                         continue;
                     }
-                    
+
                     west = westScores[game][difficulty][0];
                     westPlayer = westScores[game][difficulty][1];
                     westShottype = westScores[game][difficulty][2];
@@ -270,13 +270,13 @@ function load() {
                     "</td>" + "<th class='" + percentageClass(percentage) + "'>(" + (parseInt(percentage) == 100 ? 100 : percentage) + "%)</th></tr>");
                 }
             }
-            
+
         }, "json");
-        
+
         if (!$("#overallc").is(":checked")) {
             hide("overall");
         }
-        
+
         $("#autosort").click();
         $("#autosort").click();
     }, "json");
@@ -288,6 +288,6 @@ $(document).ready(function() {
         $("#notice").css("display", "block");
         $("#back").css("display", "block");
 	}
-    
+
 	load();
 });
