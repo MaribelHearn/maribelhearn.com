6d2941fc1d60fec0c614b379e861828a7224b313
Maribel Hearn <maribelhearn@disroot.org>
2018-12-08 13:53:51 +0100

    [Thvote] Fix mobile display

--- thvote/thvote.js
+++ thvote/thvote.js
@@ -2,5 +2,7 @@ var ready = function () {
     // detect smartphone and tablet
     if (navigator.userAgent.indexOf("Mobile") > -1 || navigator.userAgent.indexOf("Tablet") > -1) {
         document.getElementById("notice").style.display = "block";
+        document.styleSheets[0].insertRule("#wrap:before { content: ''; background-color: #FFFFFF; position: absolute; " +
+        "height: " + document.body.clientHeight + "px; width: 4000px; left: -2000px; z-index: -1; }");
 	}
 };
