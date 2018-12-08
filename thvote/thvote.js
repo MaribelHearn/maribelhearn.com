2a4fa01b8031f518cc545dc673adecbb5e16fd12
Maribel Hearn <maribelhearn@disroot.org>
2018-12-08 13:59:15 +0100

    [Thvote] Update mobile display on resize

--- thvote/thvote.js
+++ thvote/thvote.js
@@ -3,6 +3,14 @@ var ready = function () {
     if (navigator.userAgent.indexOf("Mobile") > -1 || navigator.userAgent.indexOf("Tablet") > -1) {
         document.getElementById("notice").style.display = "block";
         document.styleSheets[0].insertRule("#wrap:before { content: ''; background-color: #FFFFFF; position: absolute; " +
-        "height: " + document.body.clientHeight + "px; width: 4000px; left: -2000px; z-index: -1; }");
+        "height: " + document.body.clientHeight + "px; width: " + document.body.clientWidth + "; left: -2000px; z-index: -1; }", 0);
 	}
 };
+window.onresize = function(event) {
+    // detect smartphone and tablet
+    if (navigator.userAgent.indexOf("Mobile") > -1 || navigator.userAgent.indexOf("Tablet") > -1) {
+        document.styleSheets[0].removeRule(0);
+        document.styleSheets[0].insertRule("#wrap:before { content: ''; background-color: #FFFFFF; position: absolute; " +
+        "height: " + document.body.clientHeight + "px; width: " + document.body.clientWidth + "; left: -2000px; z-index: -1; }", 0);
+	}
+}
