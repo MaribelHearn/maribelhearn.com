5e42cd2a5a59647072fd8f4b2e885c2303a5d821
Maribel Hearn <maribelhearn@disroot.org>
2019-04-25 00:33:57 +0200

    [Tiers] Fix listCookies()

--- utils.js
+++ utils.js
@@ -1,63 +1,5 @@
 var minAge = "Thu, 01 Jan 1970 00:00:00 UTC", maxAge = "Fri, 31 Dec 9999 23:59:59 UTC";
 
-var setCookie = function (name, value) {
-    document.cookie = name + "=" + JSON.stringify(value) + ";expires=" + maxAge + ";path=/;";
-};
-
-var getCookie = function (name) {
-    var decodedCookies, cookieArray, cookie;
-
-    decodedCookies = decodeURIComponent(document.cookie);
-    cookieArray = decodedCookies.split(';');
-    name += '=';
-
-    for (var i = 0; i < cookieArray.length; i++) {
-        cookie = cookieArray[i];
-
-        while (cookie.charAt(0) === ' ') {
-            cookie = cookie.substring(1);
-        }
-
-        if (cookie.indexOf(name) === 0) {
-            return JSON.parse(cookie.substring(name.length, cookie.length));
-        }
-    }
-
-    return "";
-};
-
-var deleteCookie = function (name) {
-    document.cookie = name + "=;expires=" + minAge + ";path=/;";
-};
-
-var listCookies = function () {
-    var cookieNames = [], decodedCookies, cookieArray, i;
-
-    decodedCookies = decodeURIComponent(document.cookie);
-    cookieArray = decodedCookies.split(';');
-
-    for (i = 0; i < cookieArray.length; i++) {
-        cookieNames.push(cookieArray[i].split('=')[0]);
-    }
-
-    return cookieNames;
-};
-
-var numericSort = function (a, b) {
-    return b - a;
-};
-
-var rand = function (min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
-var sep = function (number) {
-    if (isNaN(number)) {
-        return '-';
-    }
-    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-};
-
 String.prototype.contains = function (string) {
     return this.indexOf(string) > -1;
 };
@@ -127,3 +69,61 @@ Object.defineProperty(Object.prototype, "isEmpty", {
         return Object.keys(this).length === 0;
     }
 });
+
+var setCookie = function (name, value) {
+    document.cookie = name + "=" + JSON.stringify(value) + ";expires=" + maxAge + ";path=/;";
+};
+
+var getCookie = function (name) {
+    var decodedCookies, cookieArray, cookie;
+
+    decodedCookies = decodeURIComponent(document.cookie);
+    cookieArray = decodedCookies.split(';');
+    name += '=';
+
+    for (var i = 0; i < cookieArray.length; i++) {
+        cookie = cookieArray[i];
+
+        while (cookie.charAt(0) === ' ') {
+            cookie = cookie.substring(1);
+        }
+
+        if (cookie.indexOf(name) === 0) {
+            return JSON.parse(cookie.substring(name.length, cookie.length));
+        }
+    }
+
+    return "";
+};
+
+var deleteCookie = function (name) {
+    document.cookie = name + "=;expires=" + minAge + ";path=/;";
+};
+
+var listCookies = function () {
+    var cookieNames = [], decodedCookies, cookieArray, i;
+
+    decodedCookies = decodeURIComponent(document.cookie);
+    cookieArray = decodedCookies.split(';');
+
+    for (i = 0; i < cookieArray.length; i++) {
+        cookieNames.push(cookieArray[i].split('=')[0].removeSpaces());
+    }
+
+    return cookieNames;
+};
+
+var numericSort = function (a, b) {
+    return b - a;
+};
+
+var rand = function (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+var sep = function (number) {
+    if (isNaN(number)) {
+        return '-';
+    }
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+};
