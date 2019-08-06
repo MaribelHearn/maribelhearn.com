4310322dbacc06b788f69bc49fdc1d6f2de02d83
MaribelHearn <casbas.sj@gmail.com>
2019-08-06 13:01:18 +0200

    [WR] Game colourings, update WRs

--- wr/wr.js
+++ wr/wr.js
@@ -2,7 +2,7 @@ var WRs, LIMIT = 16, playerWRs, compareWRs, westScores, seasonsEnabled, datesEna
     all = ["overall", "HRtP", "SoEW", "PoDD", "LLS", "MS", "EoSD", "PCB", "IN", "PoFV", "MoF", "SA", "UFO", "GFW", "TD", "DDC", "LoLK", "HSiFS"],
     windows = ["EoSD", "PCB", "IN", "PoFV", "MoF", "SA", "UFO", "GFW", "TD", "DDC", "LoLK", "HSiFS"],
     pc98 = ["HRtP", "SoEW", "PoDD", "LLS", "MS"],
-    lastUpdate = "July 31, 2019";
+    lastUpdate = "August 5, 2019";
 
 String.prototype.removeChar = function () {
     return this.replace("Reimu", "").replace("Cirno", "").replace("Aya", "").replace("Marisa", "");
