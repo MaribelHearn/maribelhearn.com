056b0d035de3f13211a459011a44beece1b5f7fd
Maribel Hearn <maribelhearn@disroot.org>
2019-08-11 19:47:53 +0200

    [Various] Early WBaWC on survival, delete unused scripts

--- history/history.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-	// detect smartphone and tablet
-	if (navigator.userAgent.indexOf("Mobile") > -1 || navigator.userAgent.indexOf("Tablet") > -1) {
-        $("#notice").css("display", "block");
-	}
-});
