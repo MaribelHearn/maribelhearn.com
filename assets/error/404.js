a963026255282afc6b65e965e12ee40405508470
Maribel Hearn <maribelhearn@disroot.org>
2019-09-15 19:48:34 +0200

    [All] Server-based minification

--- assets/error/404.js
+++ assets/error/404.js
@@ -1,1 +1,57 @@
-var pages=["scoring","survival","drc","tools","wr","lnn","poll","jargon","trs","tiers"];var similarity=function(a,b){var m=a.length,n=b.length,C=[],i,j;for(i=0;i<=m;i+=1){C.push([0])}for(j=0;j<n;j+=1){C[0].push(0)}for(i=0;i<m;i+=1){for(j=0;j<n;j+=1){C[i+1][j+1]=a[i]===b[j]?C[i][j]+1:Math.max(C[i+1][j],C[i][j+1])}}return(function bt(i,j){if(i*j===0){return ""}if(a[i-1]===b[j-1]){return bt(i-1,j-1)+a[i-1]}return(C[i][j-1]>C[i-1][j])?bt(i,j-1):bt(i-1,j)}(m,n)).length};var path=location.pathname.split('/').pop(),loc=location.toString(),max=0,maxPath,sim,i;if(isNaN(path)&&path!="404.html"){for(i=0;i<pages.length;i+=1){sim=similarity(path,pages[i]);if(sim>max){max=sim;maxPath=pages[i]}}loc=(loc.indexOf("file:///")>-1?loc.replace(path,maxPath)+".html":maxPath);if(max>maxPath.length-2){location.replace(loc.replace(path,maxPath));}}var load=function(){if(isNaN(path)&&path!="404.html"&&max<=maxPath.length-2){document.getElementById("didyoumean").innerHTML=", did you mean <a href='/"+loc+"'>"+maxPath+"</a>?"}};
+var pages = ["scoring", "survival", "drc", "tools", "wr", "lnn", "poll", "jargon", "trs", "tiers"];
+
+function similarity(a, b) {
+    var m = a.length, n = b.length, C = [], i, j;
+
+    for (i = 0; i <= m; i += 1) {
+        C.push([0]);
+    }
+
+    for (j = 0; j < n; j += 1) {
+        C[0].push(0);
+    }
+
+    for (i = 0; i < m; i += 1) {
+        for (j = 0; j < n; j += 1) {
+            C[i + 1][j + 1] = a[i] === b[j]
+                ? C[i][j] + 1
+                : Math.max(C[i + 1][j], C[i][j + 1])
+        }
+    }
+
+    return (function bt(i, j) {
+        if (i * j === 0) {
+            return ""
+        }
+        if (a[i - 1] === b[j - 1]) {
+            return bt(i - 1, j - 1) + a[i - 1]
+        }
+        return (C[i][j - 1] > C[i - 1][j])
+            ? bt(i, j - 1)
+            : bt(i - 1, j)
+    }(m, n)).length;
+}
+
+function load() {
+    if (isNaN(path) && path != "404.html" && max <= maxPath.length - 2) {
+        document.getElementById("didyoumean").innerHTML = ", did you mean <a href='/" + loc + "'>" + maxPath + "</a>?";
+    }
+}
+
+var path = location.pathname.split('/').pop(), loc = location.toString(), max = 0, maxPath, sim, i;
+
+if (isNaN(path) && path != "404.html") {
+    for (i = 0; i < pages.length; i += 1) {
+        sim = similarity(path, pages[i]);
+        if (sim > max) {
+            max = sim;
+            maxPath = pages[i];
+        }
+    }
+
+    loc = (loc.indexOf("file:///") > -1 ? loc.replace(path, maxPath) + ".html" : maxPath);
+
+    if (max > maxPath.length - 2) {
+        location.replace(loc.replace(path, maxPath));
+    }
+}
