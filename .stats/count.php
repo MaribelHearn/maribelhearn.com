552b7c2931e7036de41a515cc4fcbd41d373d870
Maribel Hearn <maribelhearn@disroot.org>
2020-01-17 19:40:44 +0100

    [Count] Bot checker

--- .stats/count.php
+++ .stats/count.php
@@ -1,9 +1,11 @@
 <?php
 function hit($page) {
     if (file_exists('.stats/token')) {
-        $usr = $_SERVER['HTTP_USER_AGENT'];
+        //$bot = strpos($usr, 'bot') || strpos($usr, 'Google') || strpos($usr, 'W3C') || strpos($_SERVER['HTTP_REFERER'], 'developers.google');
+        if (!empty($_SERVER['HTTP_USER_AGENT']) and preg_match('~(bot|crawl)~i', $_SERVER['HTTP_USER_AGENT'])) {
+            return;
+        }
         $token = trim(file_get_contents('.stats/token'));
-        $bot = strpos($usr, 'bot') || strpos($usr, 'Google') || strpos($usr, 'W3C') || strpos($_SERVER['HTTP_REFERER'], 'developers.google');
         if ($_SERVER['SERVER_NAME'] !== 'localhost' && $_COOKIE['token'] !== $token && $bot === false) {
             $page = str_replace('.php', '', $page);
             $hitcount = '.stats/' . date('d-m-Y') . '.json';
