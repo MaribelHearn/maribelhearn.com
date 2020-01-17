b7bed1de6947cab64d83427ad57bfb63fc82a6ef
MaribelHearn <casbas.sj@gmail.com>
2020-01-17 19:00:42 +0100

    [Count] Exclude W3C

--- .stats/count.php
+++ .stats/count.php
@@ -2,7 +2,7 @@
 function hit($page) {
     if (file_exists('.stats/token')) {
         $token = trim(file_get_contents('.stats/token'));
-        $bot = strpos($_SERVER['HTTP_USER_AGENT'], 'bot') || strpos($_SERVER['HTTP_USER_AGENT'], '-Google') || strpos($_SERVER['HTTP_REFERER'], 'developers.google');
+        $bot = strpos($_SERVER['HTTP_USER_AGENT'], 'bot') || strpos($_SERVER['HTTP_USER_AGENT'], '-Google') || strpos($_SERVER['HTTP_USER_AGENT'], 'w3c') || strpos($_SERVER['HTTP_REFERER'], 'developers.google');
         if ($_SERVER['SERVER_NAME'] !== 'localhost' && $_COOKIE['token'] !== $token && $bot === false) {
             $page = str_replace('.php', '', $page);
             $hitcount = '.stats/' . date('d-m-Y') . '.json';
