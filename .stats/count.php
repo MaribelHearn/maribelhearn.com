e115ab9e4e423408e30512fb309565cc4af081be
Maribel Hearn <maribelhearn@disroot.org>
2020-01-17 19:21:32 +0100

    [Count] Update exclusion list

--- .stats/count.php
+++ .stats/count.php
@@ -1,8 +1,9 @@
 <?php
 function hit($page) {
     if (file_exists('.stats/token')) {
+        $usr = $_SERVER['HTTP_USER_AGENT']
         $token = trim(file_get_contents('.stats/token'));
-        $bot = strpos($_SERVER['HTTP_USER_AGENT'], 'bot') || strpos($_SERVER['HTTP_USER_AGENT'], '-Google') || strpos($_SERVER['HTTP_USER_AGENT'], 'w3c') || strpos($_SERVER['HTTP_REFERER'], 'developers.google');
+        $bot = strpos($usr, 'bot') || strpos($usr, 'Google') || strpos($usr, 'W3C') || strpos($_SERVER['HTTP_REFERER'], 'developers.google');
         if ($_SERVER['SERVER_NAME'] !== 'localhost' && $_COOKIE['token'] !== $token && $bot === false) {
             $page = str_replace('.php', '', $page);
             $hitcount = '.stats/' . date('d-m-Y') . '.json';
