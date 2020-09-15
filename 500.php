300b85ddf8b511f3ff376b00fdd2449eb341e72d
MaribelHearn <casbas.sj@gmail.com>
2020-09-15 12:04:38 +0200

    [Error] Fix error page URLs

--- 500.php
+++ 500.php
@@ -9,15 +9,15 @@
 		<link rel='icon' type='image/x-icon' href='https://maribelhearn.com/favicon.ico'>
     </head>
 
-    <body class='<?php include '.stats/count.php'; echo check_webp() ?>'>
+    <body class='<?php include 'https://maribelhearn.com/.stats/count.php'; echo check_webp() ?>'>
         <main>
             <div id='wrap'>
-                <img id='hy' src='assets/shared/h-bar.png' title='Human Mode'>
+                <img id='hy' src='https://maribelhearn.com/assets/shared/h-bar.png' title='Human Mode'>
                 <h1>500</h1>
                 <p><strong>Internal Server Error</strong></p>
                 <p>The server encountered an internal error or misconfiguration and was unable to complete your request.</p>
                 <p><a id='backtomain' href='/'>Back to Main Page</a></p>
-                <script src='assets/shared/dark.js'></script>
+                <script src='https://maribelhearn.com/assets/shared/dark.js'></script>
             </div>
         </main>
     </body>
