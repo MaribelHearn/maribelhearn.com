e3ed8079d8feebbc872572fab366a37a96d702e6
MaribelHearn <casbas.sj@gmail.com>
2020-09-26 16:33:02 +0200

    [Error] Move errors to assets

--- assets/error/404.js
+++ assets/error/404.js
@@ -47,7 +47,7 @@ if (isNaN(path) && path != "404.php") {
 
     if (max > maxPath.length - 2) { // redirect
         location.replace(loc.replace(path, maxPath) + "?redirect=" + path);
-    } else if (isNaN(path) && path != "404.php" && max <= maxPath.length - 2) {
+    } else if (isNaN(path) && path != "404.php" && max >= maxPath.length - 3) {
         document.getElementById("didyoumean").innerHTML = ", did you mean <a href='/" + loc + "'>" + maxPath + "</a>?";
     }
 }
