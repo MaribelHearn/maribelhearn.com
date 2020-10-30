3ee2a1d0fb42161a3986d51819293ba308aa1dd6
Maribel Hearn <maribelhearn@disroot.org>
2020-10-30 21:26:39 +0100

    [Error] Add error 400 page

---
+++ assets/error/400.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang='en'>
+<?php
+    include '../shared/shared.php';
+    hit(basename(__FILE__));
+?>
+
+    <head>
+		<title>400 Bad Request</title>
+		<meta charset='UTF-8'>
+		<meta name='viewport' content='width=device-width'>
+        <link rel='stylesheet' type='text/css' href='https://maribelhearn.com/assets/index/main.css'>
+		<link rel='icon' type='image/x-icon' href='https://maribelhearn.com/favicon.ico'>
+        <?php echo dark_theme('error') ?>
+    </head>
+
+    <body class='<?php echo check_webp() ?>'>
+		<nav>
+            <div id='nav' class='wrap'><?php echo navbar('error') ?></div>
+		</nav>
+        <main>
+            <div id='wrap' class='wrap'>
+                <p id='ack_admin'>This background image<br id='ack_br'>
+                was drawn by <a href='https://www.pixiv.net/member.php?id=420928'>LM7</a></p>
+                <span id='hy_container'><img id='hy' src='https://maribelhearn.com/assets/shared/icon_sheet.png' alt='Human-youkai gauge'>
+                    <span id='hy_tooltip' class='tooltip'><?php echo theme_name() ?></span>
+                </span>
+                <h1>400</h1>
+                <p><strong>Bad Request</strong></p>
+                <p class='wide'>Your browser sent a request that this server could not understand.</p>
+                <p id='ack_mobile'>The background image was drawn by <a href='https://www.pixiv.net/member.php?id=420928'>LM7</a>.</p>
+                <script src='https://maribelhearn.com/assets/shared/dark.js'></script>
+            </div>
+        </main>
+    </body>
+
+</html>
