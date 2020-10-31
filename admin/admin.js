87b79462d00e8aa374a0f8d1d43a5d63ae10d737
Maribel Hearn <maribelhearn@disroot.org>
2020-10-31 17:40:29 +0100

    [Index/Admin] No floating gauge on mobile

--- admin/admin.js
+++ admin/admin.js
@@ -1,5 +1,5 @@
 function set() {
-    setCookie("token", document.getElementById("token").value);
+    document.cookie = "token=" + document.getElementById("token").value + ";expires=Fri, 31 Dec 9999 23:59:59 UTC;path=/;sameSite=Strict;Secure;";
     alert("Blocking cookie set!");
 }
 
