0c4242096bfae74b17c5988b996e0f81b6811a02
MaribelHearn <casbas.sj@gmail.com>
2020-11-01 13:58:08 +0100

    [C67] Fix typo

--- c67.php
+++ c67.php
@@ -200,7 +200,7 @@
                 <h3 id='vivita'>VIVIT-A</h3>
                 <picture>
                     <source srcset='assets/c67/vivita.webp' type='image/webp'>
-                    <source loading='lazy' loading='lazy' src='assets/c67/vivita.jpg'>
+                    <source src='assets/c67/vivita.jpg'>
                     <img loading='lazy' class='shotinfo' src='assets/c67/vivita.jpg' alt='VIVIT-A Shottype Info' width='640' height='480'>
                 </picture>
                 <br>
