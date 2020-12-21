e9cf46be03a614a126b451c9ed908953ec9a7033
MaribelHearn <casbas.sj@gmail.com>
2020-12-21 15:15:57 +0100

    [All] Use minified files for concatenation

--- assets/shared/css_concat.php
+++ assets/shared/css_concat.php
@@ -2,8 +2,8 @@
 header('Content-type: text/css');
 $page = $_GET['page'];
 $css = array(
-    'shared.css',
-    '../' . $page . '/' . ($page == 'index' ? 'main' : $page) . '.css'
+    'shared-min.css',
+    '../' . $page . '/' . ($page == 'index' ? 'main' : $page) . '-min.css'
 );
 foreach ($css as $css_file) {
     $css_content = file_get_contents($css_file);
