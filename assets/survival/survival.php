b8006441e1c11bdf94cdc7ebc5a5a35d8c34ce7b
Maribel Hearn <maribelhearn@disroot.org>
2021-05-14 13:35:29 +0200

    [Survival] Add UM and IN finals

--- assets/survival/survival.php
+++ assets/survival/survival.php
@@ -1,6 +1,6 @@
 <?php
-$games = ['HRtP', 'SoEW', 'PoDD', 'LLS', 'MS', 'EoSD', 'PCB', 'IN', 'PoFV',
-'MoF', 'SA', 'UFO', 'GFW', 'TD', 'DDC', 'LoLK', 'HSiFS', 'WBaWC'];
+$games = ['HRtP', 'SoEW', 'PoDD', 'LLS', 'MS', 'EoSD', 'PCB', 'INFinalA', 'INFinalB',
+'PoFV', 'MoF', 'SA', 'UFO', 'GFW', 'TD', 'DDC', 'LoLK', 'HSiFS', 'WBaWC', 'UM'];
 $diffs = ['Easy', 'Normal', 'Hard', 'Lunatic', 'Extra'];
 function achievs(string $game) {
     $achievs = ['N/A', 'Not cleared', '1cc', 'NM', 'NB'];
@@ -10,10 +10,20 @@ function achievs(string $game) {
         case 'TD': return array_merge($achievs, ['NT', 'NBNT', 'NMNBNT']);
         case 'HSiFS': return array_merge($achievs, ['NR', 'NBNR', 'NMNBNR']);
         case 'WBaWC': return array_merge($achievs, ['NHNRB', 'NBNHNRB', 'NNNN']);
+        case 'UM': return array_merge($achievs, ['NC', 'NBNC', 'NMNBNC']);
         default: return array_merge($achievs, ['NMNB']);
     }
 }
 function no_extra(string $game) {
-    return in_array($game, ['HRtP', 'PoDD']);
+    return in_array($game, ['HRtP', 'PoDD', 'INFinalB']);
+}
+function display_name(string $game) {
+    if ($game == 'INFinalA') {
+        return 'IN <span class="in_route">FinalA</span>';
+    } else if ($game == 'INFinalB') {
+        return 'IN <span class="in_route">FinalB</span>';
+    } else {
+        return $game;
+    }
 }
 ?>
