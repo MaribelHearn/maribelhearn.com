6eed0b0c55a175592c6e172c99a3b2f1bfbe30e9
Maribel Hearn <maribelhearn@disroot.org>
2021-05-16 14:53:54 +0200

    [Error] Update 404 similarity checker

--- assets/error/functions.php
+++ assets/error/functions.php
@@ -1,23 +1,5 @@
 <?php
 if (empty($_GET['error']) || $_GET['error'] == '404') {
-    $url = substr($_SERVER['REQUEST_URI'], 1);
-    if (!strpos($url, '/')) {
-        $max_sim = 0;
-        foreach (glob('../../*') as $file) {
-            if (strpos($file, '.php')) {
-                $page = substr($file, 6, -4);
-                $max_sim = max(similar_text($url, $page), $max_sim);
-                if (similar_text($url, $page) >= $max_sim) {
-                    $max_page = $page;
-                }
-            }
-        }
-        $len = strlen($max_page) - 3;
-        if ($max_sim > 0 && $max_sim > $len) {
-            $location = $_SERVER['SERVER_NAME'] !== 'localhost' ? 'https://maribelhearn.com/' : 'http://localhost/';
-            header('Location: ' . $location . $max_page . '?redirect=' . $url);
-        }
-    }
     $json = file_get_contents('../json/admin.json');
     $data = json_decode($json, true);
     if (isset($data[$url])) {
@@ -39,6 +21,24 @@ function error_title() {
 }
 function error_description_sub() {
     $description = '404 Not Found';
+    $url = substr($_SERVER['REQUEST_URI'], 1);
+    if (!strpos($url, '/')) {
+        $max_sim = 0;
+        foreach (glob('../../*') as $file) {
+            if (strpos($file, '.php')) {
+                $page = substr($file, 6, -4);
+                $max_sim = max(similar_text($url, $page), $max_sim);
+                if (similar_text($url, $page) >= $max_sim) {
+                    $max_page = $page;
+                }
+            }
+        }
+        $len = strlen($max_page) - 3;
+        if ($max_sim > 0 && $max_sim > $len) {
+            $location = $_SERVER['SERVER_NAME'] !== 'localhost' ? 'https://maribelhearn.com/' : 'http://localhost/';
+            header('Location: ' . $location . $max_page . '?redirect=' . $url);
+        }
+    }
     if ($max_sim > 0 && $max_sim > $len - 2) {
         $description .= ' - did you mean <a href="https://maribelhearn.com/' . $max_page . '">' . $max_page . '</a>?';
     }
