c56188a5aea48855ed712f99b42db71d0ddbf85d
Maribel Hearn <maribelhearn@disroot.org>
2021-06-26 15:46:51 +0200

    [Admin] Restructure functions

--- admin/cache.php
+++ admin/cache.php
@@ -1,9 +1,7 @@
 <?php
 $CACHE_FILE = '../.stats/cache';
-$API_KEY = trim(str_replace(array('\r', '\n'), '', file_get_contents('../.stats/key')));
-$URL = 'https://api.ipinfodb.com/v3/ip-city/?key=' . $API_KEY . '&ip=%i&format=json';
 
-function download_content($url) {
+function download_content(string $url) {
     $ch = curl_init();
     $timeout = 5;
     curl_setopt($ch, CURLOPT_URL, $url);
@@ -29,6 +27,38 @@ function format_country(string $country) {
     }
 }
 
+function fetch_country(string $ip) {
+    $API_KEY = trim(str_replace(array('\r', '\n'), '', file_get_contents('../.stats/key')));
+    $URL = 'https://api.ipinfodb.com/v3/ip-city/?key=' . $API_KEY . '&ip=%i&format=json';
+    $fetch_url = str_replace('%i', $ip, $URL);
+    $json = download_content($fetch_url);
+    if ($json) {
+        $data = json_decode($json, false);
+        if ($data->statusCode == 'OK') {
+            $country = format_country($data->countryName);
+            echo 'Cached ' . $ip . '<br>';
+            return $country;
+        } else {
+            echo 'Error while caching ' . $ip . '<br>';
+        }
+    } else {
+        echo 'Error while fetching country for ' . $ip . '<br>';
+    }
+}
+
+function add_cache_entries(string $path, object $cache, string $entries) {
+    $entries = preg_split('/,/', $entries);
+    foreach ($entries as $key => $entry) {
+        if (property_exists($cache, $entry)) {
+            echo 'Already cached ' . $entry . '<br>';
+            continue;
+        }
+        $cache->{$entry} = fetch_country($entry);
+    }
+    $file = fopen($path, 'w');
+    fwrite($file, json_encode($cache));
+}
+
 if (file_exists($CACHE_FILE)) {
     $json = file_get_contents($CACHE_FILE);
     $cache = (object) json_decode($json, true);
@@ -37,31 +67,7 @@ if (file_exists($CACHE_FILE)) {
 }
 
 if ($_GET['entries']) {
-    var_dump($_GET['entries']);
-    echo '<br>';
-    $entries = preg_split('/,/', $_GET['entries']);
-    foreach ($entries as $key => $entry) {
-        if (property_exists($cache, $entry)) {
-            echo 'Already cached ' . $entry . '<br>';
-            continue;
-        }
-        $current_url = str_replace('%i', $entry, $URL);
-        $json = download_content($current_url);
-        if ($json) {
-            $data = json_decode($json, true);
-            $data = (object) $data;
-            if ($data->statusCode == 'OK') {
-                $cache->{$entry} = format_country($data->countryName);
-                echo 'Cached ' . $entry . '<br>';
-            } else {
-                echo 'Error while caching ' . $entry . '<br>';
-            }
-        } else {
-            echo 'Error while fetching country for ' . $entry . '<br>';
-        }
-    }
-    $file = fopen($CACHE_FILE, 'w');
-    fwrite($file, json_encode($cache));
+    add_cache_entries($CACHE_FILE, $cache, $_GET['entries']);
 } else {
     echo 'No new cache entries.';
 }
