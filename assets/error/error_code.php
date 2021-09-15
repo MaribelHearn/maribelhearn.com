cc328b2f391bd8c48fd32ec62fc8db491140107e
Maribel Hearn <maribelhearn@disroot.org>
2021-09-15 19:26:45 +0200

    [Error] Fix error system

--- assets/error/error_code.php
+++ assets/error/error_code.php
@@ -1,36 +1,4 @@
 <?php
-$url = substr($_SERVER['REQUEST_URI'], 1);
-if (empty($_GET['error']) || $_GET['error'] == '404') {
-    if (file_exists('../json/admin.json')) {
-        $json = file_get_contents('../json/admin.json');
-    } else {
-        $json = file_get_contents('assets/json/admin.json');
-    }
-    $data = json_decode($json, true);
-    if (isset($data[$url])) {
-        header('Location: ' . $data[$url]);
-        exit();
-    }
-    if (!strpos($url, '/')) {
-        $min_distance = PHP_INT_MAX;
-        foreach (glob('../../*') as $file) {
-            if (strpos($file, '.php')) {
-                $page = substr($file, 6, -4);
-                $min_distance = min(levenshtein($url, $page), $min_distance);
-                if (levenshtein($url, $page) <= $min_distance) {
-                    $min_page = $page;
-                }
-            }
-        }
-        if ($min_distance < 3 && $min_distance >= 0) {
-            echo 'Redirect triggered.';
-            var_dump($min_page);
-            var_dump($min_distance);
-            $location = $_SERVER['SERVER_NAME'] !== 'localhost' ? 'https://maribelhearn.com/' : 'http://localhost/';
-            header('Location: ' . $location . $min_page . '?redirect=' . $url);
-        }
-    }
-}
 function error_title() {
     switch ($_GET['error']) {
         case '400': return '400 Bad Request';
