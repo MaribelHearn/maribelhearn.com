e954cacf0023ff722ee22394728c1b544a2faa1c
Maribel Hearn <maribelhearn@disroot.org>
2022-01-06 23:38:20 +0100

    [Royalflare] Fix Uwabami Breakers sorting

--- assets/royalflare/subpages/alcostg.php
+++ assets/royalflare/subpages/alcostg.php
@@ -5,7 +5,7 @@
     echo '<table class="alcostg sortable"><tr><th id="head">#</th><th id="score">スコア<br>Score</th><th>処理落率<br>Slowdown</th>' .
     '<th><span class="nowrap">プレイ日付</span><br>Date</th><th>名前<br>Player</th><th>コメント<br>Comment</th><th>リプレイ<br>Replay</th></tr>';
     foreach ($board as $key => $entry) {
-        echo '<tr><td>' . number_format($entry['score'], 0, '.', ',') . '</td><td>' . $entry['slowdown'] . '</td><td>' . $entry['date'] . '</td><td>' . $entry['player'] . '</td><td>' . $entry['comment'] .
+        echo '<tr><td class="hidden"><td>' . number_format($entry['score'], 0, '.', ',') . '</td><td>' . $entry['slowdown'] . '</td><td>' . $entry['date'] . '</td><td>' . $entry['player'] . '</td><td>' . $entry['comment'] .
         '</td><td><a href="' . $entry['replay'] . '">' . array_slice(preg_split('/\//', $entry['replay']), -1)[0] . '</a></td></tr>';
     }
     echo '</table>';
