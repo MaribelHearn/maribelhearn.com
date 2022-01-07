72a98bef6a817c82b8a1c72104ace64145d2527f
Maribel Hearn <maribelhearn@disroot.org>
2022-01-07 02:09:20 +0100

    [Royalflare] Upload dates in search, fix TD replay links

--- assets/royalflare/subpages/search.php
+++ assets/royalflare/subpages/search.php
@@ -94,7 +94,7 @@
                 $table .= '<tr><td class="hidden"></td>' . ($gamecol ? '<td class=' . game_to_abbr($game) . '>' . $game . '</td>' : '') .
                 '<td>' . number_format($entry['score'], 0, '.', ',') . '</td><td>' . $entry['slowdown'] . '</td><td>' . $tmp_shot . '</td>' .
                 '<td>' . $tmp_diff . '</td><td>' . $entry['date'] . '</td><td>' . $entry['player'] .
-                '</td><td class="break">' . $entry['comment'] . '</td><td><a href="' . $entry['replay'] . '">' . array_slice(preg_split('/\//', $entry['replay']), -1)[0] . '</a></td></tr>';
+                '</td><td class="break">' . $entry['comment'] . '</td><td><a href="' . $entry['replay'] . '">' . $entry['uploaded'] . '</a></td></tr>';
             }
         }
         return $table;
