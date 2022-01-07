89e8d495d29069188f0361bb1fbc5c2995f72c81
MaribelHearn <casbas.sj@gmail.com>
2022-01-07 19:46:10 +0100

    [Royalflare] Minor fix

--- assets/royalflare/royalflare_code.php
+++ assets/royalflare/royalflare_code.php
@@ -143,11 +143,11 @@ if (!empty($_SESSION['subpage'])) {
         $shots = get_shots($game);
     }
     if ($subpage == 'search') {
-        $player = (isset($_GET['player']) ? preg_replace('/</', '&lt;', $_GET['player']) : '');
-        $game = (isset($_GET['game']) ? preg_replace('/</', '&lt;', $_GET['game']) : '');
-        $diff = (isset($_GET['diff']) ? preg_replace('/</', '&lt;', $_GET['diff']) : '');
-        $shot = (isset($_GET['shot']) ? preg_replace('/</', '&lt;', $_GET['shot']) : '');
-        $comment = (isset($_GET['comment']) ? preg_replace('/</', '&lt;', $_GET['comment']) : '');
+        $player = (isset($_GET['player']) ? preg_replace('/</', '&lt;', preg_replace('/>/', '&gt;', $_GET['player'])) : '');
+        $game = (isset($_GET['game']) ? preg_replace('/</', '&lt;', preg_replace('/>/', '&gt;', $_GET['game'])) : '');
+        $diff = (isset($_GET['diff']) ? preg_replace('/</', '&lt;', preg_replace('/>/', '&gt;', $_GET['diff'])) : '');
+        $shot = (isset($_GET['shot']) ? preg_replace('/</', '&lt;', preg_replace('/>/', '&gt;', $_GET['shot'])) : '');
+        $comment = (isset($_GET['comment']) ? preg_replace('/</', '&lt;', preg_replace('/>/', '&gt;', $_GET['comment'])) : '');
         if ($game == 'th095' || $game == 'th125' || $game == 'th143' || $game == 'th165') {
             $diff = '-';
         }
