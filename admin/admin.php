ef5ad1c9cfb55cdd6067502bdb801a7ab65c023d
Maribel Hearn <maribelhearn@disroot.org>
2022-02-03 22:35:07 +0100

    [Admin] Fix JSON path bug

--- admin/admin.php
+++ admin/admin.php
@@ -35,6 +35,7 @@
     }
     $flag_url = 'https://icons.iconarchive.com/icons/custom-icon-design/all-country-flag/16/';
     $lang_code = 'en';
+    $page = 'admin';
     function cmp(int $a, int $b) {
         if ($a == $b) {
             return 0;
