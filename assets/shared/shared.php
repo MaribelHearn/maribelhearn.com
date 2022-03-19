5c3614a07254a1bcb096f7bcc0eca6cac3eaa51e
Maribel Hearn <maribelhearn@disroot.org>
2022-03-19 23:10:02 +0100

    [Tiers] Move uploaded file correct path

--- assets/shared/shared.php
+++ assets/shared/shared.php
@@ -439,7 +439,7 @@ function handle_file_upload() {
             if (!empty($ext)) {
                 $ext = '.' . $ext;
             }
-            $path = sprintf('./assets' . (is_localhost($_SERVER['REMOTE_ADDR']) ? '/other' : '') . '/tiers/uploads/%s%s', sha1_file($_FILES['import']['tmp_name']), $ext);
+            $path = sprintf('./assets/other/tiers/uploads/%s%s', sha1_file($_FILES['import']['tmp_name']), $ext);
             if (!move_uploaded_file($_FILES['import']['tmp_name'], $path)) {
                 return '<strong class="error">Error: failed to move uploaded file.</strong>';
             }
