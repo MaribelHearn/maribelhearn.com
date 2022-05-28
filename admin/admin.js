5a20a7a6bb802782295620a838cc97fb1d776d23
Maribel Hearn <maribelhearn@disroot.org>
2022-05-28 15:03:53 +0200

    [Various] Merge dark.js with utils.js, use everywhere

--- admin/admin.js
+++ admin/admin.js
@@ -1,20 +1,6 @@
 /*global detect*/
-var head = document.getElementsByTagName("head")[0], done = false;
-
-function expandCountries() {
-    var elements = document.getElementsByTagName('*'), i;
-
-    for (i in elements) {
-        if (elements[i].className && elements[i].className == "hidden") {
-            elements[i].style = "display: table-row";
-        }
-    }
-
-    document.getElementById("expand").style = "display: none";
-}
-
 function set() {
-    var cookieString = ";expires=Fri, 31 Dec 9999 23:59:59 UTC;path=/;sameSite=Strict;";
+    let cookieString = ";expires=Fri, 31 Dec 9999 23:59:59 UTC;path=/;sameSite=Strict;";
 
     if (location.protocol == "https:") {
         cookieString += "Secure;";
@@ -24,80 +10,13 @@ function set() {
     alert("Blocking cookie set!");
 }
 
-function dark() {
-    var style = document.createElement("link");
-
-    style.id = "dark_theme";
-    style.href = (location.host == "maribelhearn.com" ? "https://maribelhearn.com/" : "") + "../assets/shared/dark.css";
-    style.type = "text/css";
-    style.rel = "stylesheet";
-    head.appendChild(style);
-}
-
-function ready() {
-    if (done) {
-        return;
-    }
-
-    done = true;
-
-    if (localStorage.theme == "dark") { // legacy
-        document.cookie = "theme=dark;expires=Fri, 31 Dec 9999 23:59:59 UTC;path=/;sameSite=Strict;Secure;";
-        localStorage.removeItem("theme");
-        document.getElementById("hy_tooltip").innerHTML = "Youkai Mode";
-        dark();
-    }
-}
-
-function theme() {
-    if (document.head.contains(document.getElementById("dark_theme")) || localStorage.theme == "dark") {
-        if (document.head.contains(document.getElementById("dark_theme"))) {
-            head.removeChild(document.getElementById("dark_theme"));
-        }
-
-        document.cookie = "theme=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/;" +
-        "sameSite=Strict;" + (location.protocol == "https:" ? "Secure;" : "");
-        document.getElementById("hy_tooltip").innerHTML = "Human Mode";
-    } else {
-        var cookieString = ";expires=Fri, 31 Dec 9999 23:59:59 UTC;path=/;sameSite=Strict;";
-
-        if (location.protocol == "https:") {
-            cookieString += "Secure;";
-        }
-
-        document.cookie = "theme=" + JSON.stringify("dark") + cookieString;
-        document.getElementById("hy_tooltip").innerHTML = "Youkai Mode";
-        dark();
-    }
-
-    if (localStorage.theme) { // legacy
-        localStorage.removeItem("theme");
-    }
-}
-
 function userAgent() {
-    var ua = detect.parse(navigator.userAgent);
+    const ua = detect.parse(navigator.userAgent);
 
     document.getElementById("os").innerHTML = "<img src='" + (location.host == "maribelhearn.com" ? "https://maribelhearn.com/" : "") +
     "admin/icons/" + ua.os.name + ".png' alt='" + ua.os.name + " icon'> " + ua.os.name;
     document.getElementById("browser").innerHTML = "<img src='" + (location.host == "maribelhearn.com" ? "https://maribelhearn.com/" : "") +
     "admin/icons/" + ua.browser.family + ".png' alt='" + ua.browser.name + " icon'> " + ua.browser.name;
-}
-
-function addEventListeners() {
-    var hy = document.getElementById("hy"), expand = document.getElementById("expand");
-
-    document.getElementById("setcookie").addEventListener("click", set);
-    window.addEventListener("load", ready);
-    done = false;
-
-    if (expand) {
-        expand.addEventListener("click", expandCountries);
-    }
-
-    if (hy) {
-        hy.addEventListener("click", theme);
-    }
 }
 
 function addCacheEntries() {
@@ -114,6 +33,6 @@ function addCacheEntries() {
     }
 }
 
-userAgent();
-addEventListeners();
+document.getElementById("setcookie").addEventListener("click", set);
 addCacheEntries();
+userAgent();
--- assets/shared/js_concat.php
+++ assets/shared/js_concat.php
@@ -43,22 +43,18 @@ function directory(string $page) {
 $min = (!is_localhost($_SERVER['REMOTE_ADDR']) ? '-min' : '');
 $sorttable = array('drc', 'fangame', 'gensokyo', 'lnn', 'royalflare', 'scoring', 'survival', 'thvote', 'wr');
 $jquery = array('drc', 'lnn', 'pofv', 'royalflare', 'scoring', 'slots', 'survival', 'tiers', 'wr');
-$utils = array('drc', 'lnn', 'pofv', 'scoring', 'slots', 'survival', 'tiers', 'wr');
 $canvas = array('slots', 'survival', 'tiers');
 $wr_json = array('drc', 'scoring', 'wr');
 $po2json = array('drc', 'lnn', 'wr');
 $page = $_GET['page'];
 $dir = directory($page);
-$js = array();
+$js = array('js/utils' . $min . '.js');
 if (in_array($page, $canvas)) {
     array_push($js, 'js/html2canvas' . $min . '.js');
     array_push($js, 'js/polyfill_promise' . $min . '.js');
 }
 if (in_array($page, $jquery)) {
     array_push($js, 'js/jquery' . $min . '.js');
-}
-if (in_array($page, $utils)) {
-    array_push($js, 'js/utils' . $min . '.js');
 }
 if (in_array($page, $sorttable)) {
     array_push($js, 'js/sorttable' . $min . '.js');
@@ -70,9 +66,6 @@ if ($page == 'tiers' && isset($_GET['mobile']) && $_GET['mobile']) {
 } else if ($page == 'admin') {
     array_push($js, '../../admin/detect.js');
     array_push($js, '../../admin/admin.js');
-    array_push($js, 'js/dark' . $min . '.js');
-} else {
-    array_push($js, 'js/dark' . $min . '.js');
 }
 if (file_exists('../' . $dir . '/' . $page . '/' . $page . $min . '.js')) {
     array_push($js, '../' . $dir . '/' . $page . '/' . $page . $min . '.js');
