640828f25865acb3f69317f252e9e80ca84c0c2c
Maribel Hearn <maribelhearn@disroot.org>
2022-06-21 23:52:08 +0200

    [Index] Privacy Policy at bottom

--- assets/main/index/index.php
+++ assets/main/index/index.php
@@ -8,39 +8,30 @@
             <h3>Games</h3>
             <div class='page_table'>
                 <p class='link'>
-                    <a href='/scoring'><span class='icon scoring_icon'></span>
-                    High Score Storage</a>
-                    <span class='tooltip'>Allows you to save your Touhou game scores,
-                    as well as calculate how they compare to the world records.</span>
+                    <a href='/scoring'><span class='icon scoring_icon'></span> High Score Storage</a>
+                    <span class='tooltip'>Allows you to save your Touhou game scores, as well as calculate how they compare to the world records.</span>
                 </p>
                 <p class='link'>
-                    <a href='/survival'><span class='icon survival_icon'></span>
-                    Survival Progress Table Generator</a>
-                    <span class='tooltip'>Tool that lets you generate a table that
-                    summarises your Touhou survival progress.</span>
+                    <a href='/survival'><span class='icon survival_icon'></span> Survival Progress Table Generator</a>
+                    <span class='tooltip'>Tool that lets you generate a table that summarises your Touhou survival progress.</span>
                 </p>
                 <p class='link'>
-                    <a href='/drc'><span class='icon drc_icon'></span>
-                    Dodging Rain Competition</a>
+                    <a href='/drc'><span class='icon drc_icon'></span> Dodging Rain Competition</a>
                     <span class='icon flag_japan'></span>
                     <span class='icon flag_china'></span>
                     <span class='icon flag_germany'></span>
-                    <span class='tooltip'>Webpage intended for use by
-                    the Dodging Rain Competition (see page for explanation).</span>
+                    <span class='tooltip'>Webpage intended for use by the Dodging Rain Competition (see page for explanation).</span>
                 </p>
                 <p class='link'>
-                    <a href='/tools'><span class='icon tools_icon'></span>
-                    Touhou Patches and Tools</a>
+                    <a href='/tools'><span class='icon tools_icon'></span> Touhou Patches and Tools</a>
                     <span class='icon flag_japan'></span>
                     <span class='icon flag_russia'></span>
                     <span class='icon flag_germany'></span>
                     <span class='icon flag_spain'></span>
-                    <span class='tooltip'>A collection of helpful patches and tools for
-                    the Touhou shooting games, many of which for efficient practicing.</span>
+                    <span class='tooltip'>A collection of helpful patches and tools for the Touhou shooting games, many of which for efficient practicing.</span>
                 </p>
                 <p class='link'>
-                    <a href='/wr'><span class='icon wr_icon'></span>
-                    Touhou World Records</a>
+                    <a href='/wr'><span class='icon wr_icon'></span> Touhou World Records</a>
                     <span class='icon flag_japan'></span>
                     <span class='icon flag_china'></span>
                     <span class='icon flag_russia'></span>
@@ -49,8 +40,7 @@
                     <span class='tooltip'>The world records for all Touhou shooting games except for the scene games.</span>
                 </p>
                 <p class='link'>
-                    <a href='/lnn'><span class='icon lnn_icon'></span>
-                    Touhou Lunatic No Miss No Bombs</a>
+                    <a href='/lnn'><span class='icon lnn_icon'></span> Touhou Lunatic No Miss No Bombs</a>
                     <span class='icon flag_japan'></span>
                     <span class='icon flag_china'></span>
                     <span class='icon flag_russia'></span>
@@ -61,38 +51,31 @@
                 </p>
                 <p class='link'>
                     <a href='/jargon'><span class='icon jargon_icon'></span> Touhou Community Jargon</a>
-                    <span class='tooltip'>List of terminology used by the Touhou community,
-                    including but not limited to common acronyms for Spell Cards.</span>
+                    <span class='tooltip'>List of terminology used by the Touhou community, including but not limited to common acronyms for Spell Cards.</span>
                 </p>
                 <p class='link'>
-                    <a href='/trs'><span class='icon trs_icon'></span>
-                    Touhou Replay Showcase</a>
+                    <a href='/trs'><span class='icon trs_icon'></span> Touhou Replay Showcase</a>
                     <span class='tooltip'>Read about the weekly Touhou Replay Showcase Twitch streams.</span>
                 </p>
                 <p class='link'>
-                    <a href='/gensokyo'><span class='icon gensokyo_icon'></span>
-                    Gensokyo Replay Archive</a>
+                    <a href='/gensokyo'><span class='icon gensokyo_icon'></span> Gensokyo Replay Archive</a>
                     <span class='tooltip'>A complete and searchable archive of the replays from replays.gensokyo.org.</span>
                 </p>
                 <p class='link'>
-                    <a href='/pofv'><span class='icon pofv_icon'></span>
-                    Phantasmagoria of Flower View</a>
+                    <a href='/pofv'><span class='icon pofv_icon'></span> Phantasmagoria of Flower View</a>
                     <span class='icon flag_china'></span>
                     <span class='tooltip'>Portal for competitive PoFV play, featuring info about its metagame and tournaments and links to relevant resources.</span>
                 </p>
                 <p class='link'>
-                    <a href='/fangame'><span class='icon fangame_icon'></span>
-                    Fangame Accomplishments</a>
+                    <a href='/fangame'><span class='icon fangame_icon'></span> Fangame Accomplishments</a>
                     <span class='tooltip'>Notable accomplishments in Touhou fangames and Touhou-related games, including both LNNs and score runs.</span>
                 </p>
                 <p class='link'>
-                    <a href='/faq'><span class='icon faq_icon'></span>
-                    Frequently Asked Questions</a>
+                    <a href='/faq'><span class='icon faq_icon'></span> Frequently Asked Questions</a>
                     <span class='tooltip'>Answers to some of the most frequently asked questions about the Touhou shoot 'em up games by beginning players.</span>
                 </p>
                 <p class='link'>
-                    <a href='/royalflare'><span class='icon royalflare_icon'></span>
-                    ロイヤルフレアの記録 Royalflare Archive</a>
+                    <a href='/royalflare'><span class='icon royalflare_icon'></span> ロイヤルフレアの記録 Royalflare Archive</a>
                     <span class='icon flag_japan'></span>
                     <span class='tooltip'>A complete and searchable archive of the replays, history and player rankings from score.royalflare.net.</span>
                 </p>
@@ -102,34 +85,31 @@
             <h3>Other</h3>
             <div class='page_table'>
                 <p class='link'>
-                    <a href='/thvote'><span class='icon thvote_icon'></span>
-                    THWiki Popularity Poll 2021 Results Translation</a>
-                    <span class='tooltip'>Complete English translation of the full
-                    results of the THWiki Popularity Poll held in 2021.</span>
+                    <a href='/thvote'><span class='icon thvote_icon'></span> THWiki Popularity Poll 2021 Results Translation</a>
+                    <span class='tooltip'>Complete English translation of the full results of the THWiki Popularity Poll held in 2021.</span>
                 </p>
                 <p class='link'>
-                    <a href='/tiers'><span class='icon tiers_icon'></span>
-                    Touhou Tier List Creator</a>
-                    <span class='tooltip'>Custom Touhou tier lists can be created here,
-                    both characters and official works.</span>
+                    <a href='/tiers'><span class='icon tiers_icon'></span> Touhou Tier List Creator</a>
+                    <span class='tooltip'>Custom Touhou tier lists can be created here, both characters and official works.</span>
                 </p>
                 <p class='link'>
                     <a href='/slots'><span class='icon slots_icon'></span> Touhou Slot Machine</a>
-                    <span class='tooltip'>Touhou randomizer based on Touhou Click and Drag Game,
-                    with customisation options.</span>
+                    <span class='tooltip'>Touhou randomizer based on Touhou Click and Drag Game, with customisation options.</span>
                 </p>
             </div>
             <h3>Personal</h3>
             <div class='page_table'>
                 <p class='link'>
-                    <a href='/history'><span class='icon history_icon'></span>
-                    Shmup Achievement History</a>
+                    <a href='/about'><span class='icon'></span> About Me</a>
+                    <span class='tooltip'>Personal introduction as well as some random facts.</span>
+                </p>
+                <p class='link'>
+                    <a href='/history'><span class='icon history_icon'></span> Shmup Achievement History</a>
                     <span class='tooltip'>Documentation of my history as a shmup player.</span>
                 </p>
                 <p class='link'>
                     <a href='/c67'><span class='icon c67_icon'></span> Seihou Banshiryuu C67</a>
-                    <span class='tooltip'>Explanation of scoring in Banshiryuu C67,
-                    the earlier version of the third Seihou game.</span>
+                    <span class='tooltip'>Explanation of scoring in Banshiryuu C67, the earlier version of the third Seihou game.</span>
                 </p>
             </div>
         </div>
@@ -144,6 +124,7 @@
         </div>
     </div>
     <div id='bottom'>
+        <p><a href='/privacy'>Privacy Policy</a></p>
         <p id='last_modified'>Last updated at <?php echo date('Y-m-d H:i:s', filemtime('.git/FETCH_HEAD')) . ' ' . date('T') ?></p>
     </div>
 </div>
