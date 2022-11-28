46b9a19e878fc28c41dd308d00829135832babf9
Maribel Hearn <maribelhearn@disroot.org>
2022-11-28 21:57:01 +0100

    [Index] Make last update date work on clone, idem for nonce

--- index.php
+++ index.php
@@ -126,9 +126,16 @@
         <?php if ($page != 'tiers') { echo '<nav data-html2canvas-ignore><div id="nav" class="wrap">' . navbar($page) . '</div></nav>'; } ?>
         <main><?php if ($page == 'error') { include_once 'php/error.php'; } else { include_once $page_path; } ?></main>
         <?php
+            // Define nonce
+            if (!file_exists('.stats/nonce')) {
+                $nonce = generate_nonce();
+            } else {
+                $nonce = file_get_contents('.stats/nonce');
+            }
+
             // Deferred page background
             if (!$is_mobile || $page != 'slots' && $page != 'thvote' && $page != 'tiers') {
-                echo '<script nonce="' . file_get_contents('.stats/nonce') . '">document.body.style.background="url(\'' . ($page == 'error' ? 'https://maribelhearn.com/' : '/') . 'assets/' . $dir . '/' . $css_js_file . '/' . $css_js_file . '.jpg\') ';
+                echo '<script nonce="' . $nonce . '">document.body.style.background="url(\'' . ($page == 'error' ? 'https://maribelhearn.com/' : '/') . 'assets/' . $dir . '/' . $css_js_file . '/' . $css_js_file . '.jpg\') ';
                 echo $bg_pos . ' no-repeat fixed";document.body.style.backgroundSize="cover"</script>';
                 echo '<noscript><link rel="stylesheet" href="/assets/shared/noscript_bg.php?page=' . $css_js_file . '&pos=' . $bg_pos . '"></noscript>';
             }
