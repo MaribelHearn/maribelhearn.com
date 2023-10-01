84e7c3795089ea63d75966cc96ebf696f5f3eacb
Maribel Hearn <maribelhearn@disroot.org>
2023-10-01 11:55:22 +0200

    [Thvote] Character vote by maingame

--- php/subpages/thvote/extras.php
+++ php/subpages/thvote/extras.php
@@ -1,9 +1,10 @@
 <h2>Contents</h2>
 <div class='contents'>
     <p><a href='#jumps'>Biggest jumps</a></p>
+    <p><a href='#cpg'>Character vote per maingame</a></p>
 </div>
 <h2 id='jumps'>Biggest jumps</h2>
-<p>This section lists the biggest jumps in ranking for characters (top 150 only). Click a column to sort the table by jumps, drops or whatever you like.</p>
+<p>This section lists the biggest jumps in ranking for characters since last year (top 150 only). Click a column to sort the table by jumps, drops or whatever you like.</p>
 <div class='overflow'><table class='poll table sortable noborders'>
     <colgroup>
         <col class='col2game'>
@@ -12,13 +13,13 @@
         <col class='col5'>
     </colgroup>
     <thead>
-        <tr><th>Current Rank</th><th>Previous Rank</th><th>Character</th><th id='change' class='sorttable_numeric'>Change</th></tr>
+        <tr><th>Current Rank</th><th>Previous Rank</th><th>Character</th><th id='change' class='sorttable_numeric'>Change from 2022</th></tr>
     </thead>
     <tbody>
-        <tr><td>1</td><td>2</td><td>Marisa Kirisame</td><td class='jump'>1</td></tr>
-        <tr><td>2</td><td>3</td><td>Reimu Hakurei</td><td class='jump'>1</td></tr>
+        <tr><td>1</td><td>2</td><td>Marisa Kirisame</td><td class='jump'>+1</td></tr>
+        <tr><td>2</td><td>3</td><td>Reimu Hakurei</td><td class='jump'>+1</td></tr>
         <tr><td>3</td><td>1</td><td>Flandre Scarlet</td><td class='drop'>-2</td></tr>
-        <tr><td>4</td><td>6</td><td>Koishi Komeiji</td><td class='jump'>2</td></tr>
+        <tr><td>4</td><td>6</td><td>Koishi Komeiji</td><td class='jump'>+2</td></tr>
         <tr><td>5</td><td>4</td><td>Youmu Konpaku</td><td class='drop'>-1</td></tr>
         <tr><td>6</td><td>5</td><td>Sakuya Izayoi</td><td class='drop'>-1</td></tr>
         <tr><td>7</td><td>7</td><td>Remilia Scarlet</td><td class='stable'>0</td></tr>
@@ -26,88 +27,88 @@
         <tr><td>9</td><td>9</td><td>Satori Komeiji</td><td class='stable'>0</td></tr>
         <tr><td>10</td><td>10</td><td>Yuyuko Saigyouji</td><td class='stable'>0</td></tr>
         <tr><td>11</td><td>11</td><td>Alice Margatroid</td><td class='stable'>0</td></tr>
-        <tr><td>12</td><td>14</td><td>Sanae Kochiya</td><td class='jump'>2</td></tr>
+        <tr><td>12</td><td>14</td><td>Sanae Kochiya</td><td class='jump'>+2</td></tr>
         <tr><td>13</td><td>12</td><td>Aya Shameimaru</td><td class='drop'>-1</td></tr>
         <tr><td>14</td><td>13</td><td>Reisen Udongein Inaba</td><td class='drop'>-1</td></tr>
         <tr><td>15</td><td>15</td><td>Cirno</td><td class='stable'>0</td></tr>
         <tr><td>16</td><td>16</td><td>Yukari Yakumo</td><td class='stable'>0</td></tr>
-        <tr><td>17</td><td>18</td><td>Tenshi Hinanawi</td><td class='jump'>1</td></tr>
-        <tr><td>18</td><td>19</td><td>Patchouli Knowledge</td><td class='jump'>1</td></tr>
+        <tr><td>17</td><td>18</td><td>Tenshi Hinanawi</td><td class='jump'>+1</td></tr>
+        <tr><td>18</td><td>19</td><td>Patchouli Knowledge</td><td class='jump'>+1</td></tr>
         <tr><td>19</td><td>17</td><td>Rumia</td><td class='drop'>-2</td></tr>
         <tr><td>20</td><td>20</td><td>Suwako Moriya</td><td class='stable'>0</td></tr>
         <tr><td>21</td><td>21</td><td>Hata no Kokoro</td><td class='stable'>0</td></tr>
-        <tr><td>22</td><td>25</td><td>Kogasa Tatara</td><td class='jump'>3</td></tr>
+        <tr><td>22</td><td>25</td><td>Kogasa Tatara</td><td class='jump'>+3</td></tr>
         <tr><td>23</td><td>23</td><td>Hong Meiling</td><td class='stable'>0</td></tr>
-        <tr><td>24</td><td>34</td><td>Ran Yakumo</td><td class='jump'>10</td></tr>
+        <tr><td>24</td><td>34</td><td>Ran Yakumo</td><td class='jump'>+10</td></tr>
         <tr><td>25</td><td>22</td><td>Eiki Shiki, Yamaxanadu</td><td class='drop'>-3</td></tr>
         <tr><td>26</td><td>26</td><td>Yuuka Kazami</td><td class='stable'>0</td></tr>
         <tr><td>27</td><td>24</td><td>Momiji Inubashiri</td><td class='drop'>-3</td></tr>
         <tr><td>28</td><td>28</td><td>Junko</td><td class='stable'>0</td></tr>
-        <tr><td>29</td><td>53</td><td>Yuuma Toutetsu</td><td class='jump'>24</td></tr>
-        <tr><td>30</td><td>31</td><td>Suika Ibuki</td><td class='jump'>1</td></tr>
+        <tr><td>29</td><td>53</td><td>Yuuma Toutetsu</td><td class='jump'>+24</td></tr>
+        <tr><td>30</td><td>31</td><td>Suika Ibuki</td><td class='jump'>+1</td></tr>
         <tr><td>31</td><td>27</td><td>Toyosatomimi no Miko</td><td class='drop'>-4</td></tr>
         <tr><td>32</td><td>30</td><td>Shion Yorigami</td><td class='drop'>-2</td></tr>
-        <tr><td>33</td><td>36</td><td>Utsuho Reiuji</td><td class='jump'>3</td></tr>
-        <tr><td>34</td><td>40</td><td>Kaguya Houraisan</td><td class='jump'>6</td></tr>
+        <tr><td>33</td><td>36</td><td>Utsuho Reiuji</td><td class='jump'>+3</td></tr>
+        <tr><td>34</td><td>40</td><td>Kaguya Houraisan</td><td class='jump'>+6</td></tr>
         <tr><td>35</td><td>32</td><td>Seija Kijin</td><td class='drop'>-3</td></tr>
         <tr><td>36</td><td>33</td><td>Sagume Kishin</td><td class='drop'>-3</td></tr>
-        <tr><td>37</td><td>38</td><td>Parsee Mizuhashi</td><td class='jump'>1</td></tr>
+        <tr><td>37</td><td>38</td><td>Parsee Mizuhashi</td><td class='jump'>+1</td></tr>
         <tr><td>38</td><td>29</td><td>Hecatia Lapislazuli</td><td class='drop'>-9</td></tr>
         <tr><td>39</td><td>37</td><td>Mononobe no Futo</td><td class='drop'>-2</td></tr>
         <tr><td>40</td><td>35</td><td>Nitori Kawashiro</td><td class='drop'>-5</td></tr>
-        <tr><td>41</td><td>45</td><td>Rin Kaenbyou</td><td class='jump'>4</td></tr>
+        <tr><td>41</td><td>45</td><td>Rin Kaenbyou</td><td class='jump'>+4</td></tr>
         <tr><td>41</td><td>41</td><td>Renko Usami</td><td class='stable'>0</td></tr>
         <tr><td>43</td><td>39</td><td>Kasen Ibaraki</td><td class='drop'>-4</td></tr>
         <tr><td>44</td><td>43</td><td>Okina Matara</td><td class='drop'>-1</td></tr>
         <tr><td>45</td><td>42</td><td>Hina Kagiyama</td><td class='drop'>-3</td></tr>
-        <tr><td>46</td><td>65</td><td>Yachie Kicchou</td><td class='jump'>19</td></tr>
-        <tr><td>47</td><td>57</td><td>Tsukasa Kudamaki</td><td class='jump'>10</td></tr>
-        <tr><td>48</td><td>49</td><td>Nazrin</td><td class='jump'>1</td></tr>
+        <tr><td>46</td><td>65</td><td>Yachie Kicchou</td><td class='jump'>+19</td></tr>
+        <tr><td>47</td><td>57</td><td>Tsukasa Kudamaki</td><td class='jump'>+10</td></tr>
+        <tr><td>48</td><td>49</td><td>Nazrin</td><td class='jump'>+1</td></tr>
         <tr><td>49</td><td>47</td><td>Byakuren Hijiri</td><td class='drop'>-2</td></tr>
-        <tr><td>50</td><td>56</td><td>Chen</td><td class='jump'>6</td></tr>
+        <tr><td>50</td><td>56</td><td>Chen</td><td class='jump'>+6</td></tr>
         <tr><td>51</td><td>44</td><td>Nue Houjuu</td><td class='drop'>-7</td></tr>
         <tr><td>52</td><td>-</td><td>Hisami Yomotsu</td><td class='stable'>0</td></tr>
         <tr><td>53</td><td>46</td><td>Eirin Yagokoro</td><td class='drop'>-7</td></tr>
-        <tr><td>54</td><td>60</td><td>Maribel Hearn</td><td class='jump'>6</td></tr>
+        <tr><td>54</td><td>60</td><td>Maribel Hearn</td><td class='jump'>+6</td></tr>
         <tr><td>55</td><td>54</td><td>Keine Kamishirasawa</td><td class='drop'>-1</td></tr>
         <tr><td>56</td><td>50</td><td>Doremy Sweet</td><td class='drop'>-6</td></tr>
         <tr><td>57</td><td>55</td><td>Koakuma</td><td class='drop'>-2</td></tr>
         <tr><td>58</td><td>51</td><td>Keiki Haniyasushin</td><td class='drop'>-7</td></tr>
         <tr><td>59</td><td>59</td><td>Minamitsu Murasa</td><td class='stable'>0</td></tr>
         <tr><td>60</td><td>58</td><td>Seiga Kaku</td><td class='drop'>-2</td></tr>
-        <tr><td>61</td><td>66</td><td>Daiyousei</td><td class='jump'>5</td></tr>
+        <tr><td>61</td><td>66</td><td>Daiyousei</td><td class='jump'>+5</td></tr>
         <tr><td>62</td><td>47</td><td>Clownpiece</td><td class='drop'>-15</td></tr>
-        <tr><td>63</td><td>69</td><td>Mystia Lorelei</td><td class='jump'>6</td></tr>
+        <tr><td>63</td><td>69</td><td>Mystia Lorelei</td><td class='jump'>+6</td></tr>
         <tr><td>64</td><td>52</td><td>Chimata Tenkyuu</td><td class='drop'>-12</td></tr>
-        <tr><td>65</td><td>71</td><td>Sekibanki</td><td class='jump'>6</td></tr>
+        <tr><td>65</td><td>71</td><td>Sekibanki</td><td class='jump'>+6</td></tr>
         <tr><td>66</td><td>61</td><td>Megumu Iizunamaru</td><td class='drop'>-5</td></tr>
         <tr><td>67</td><td>67</td><td>Tewi Inaba</td><td class='stable'>0</td></tr>
         <tr><td>68</td><td>-</td><td>Zanmu Nippaku</td><td class='stable'>0</td></tr>
-        <tr><td>69</td><td>70</td><td>Kagerou Imaizumi</td><td class='jump'>1</td></tr>
-        <tr><td>70</td><td>75</td><td>Mima</td><td class='jump'>5</td></tr>
+        <tr><td>69</td><td>70</td><td>Kagerou Imaizumi</td><td class='jump'>+1</td></tr>
+        <tr><td>70</td><td>75</td><td>Mima</td><td class='jump'>+5</td></tr>
         <tr><td>71</td><td>-</td><td>Enoko Mitsugashira</td><td class='stable'>0</td></tr>
-        <tr><td>72</td><td>76</td><td>Kosuzu Motoori</td><td class='jump'>4</td></tr>
+        <tr><td>72</td><td>76</td><td>Kosuzu Motoori</td><td class='jump'>+4</td></tr>
         <tr><td>73</td><td>62</td><td>Iku Nagae</td><td class='drop'>-11</td></tr>
         <tr><td>74</td><td>-</td><td>Chiyari Tenkajin</td><td class='stable'>0</td></tr>
-        <tr><td>75</td><td>79</td><td>Mamizou Futatsuiwa</td><td class='jump'>4</td></tr>
-        <tr><td>76</td><td>78</td><td>Kanako Yasaka</td><td class='jump'>2</td></tr>
+        <tr><td>75</td><td>79</td><td>Mamizou Futatsuiwa</td><td class='jump'>+4</td></tr>
+        <tr><td>76</td><td>78</td><td>Kanako Yasaka</td><td class='jump'>+2</td></tr>
         <tr><td>76</td><td>68</td><td>Joon Yorigami</td><td class='drop'>-8</td></tr>
         <tr><td>78</td><td>-</td><td>Son Biten</td><td class='stable'>0</td></tr>
         <tr><td>79</td><td>63</td><td>Kutaka Niwatari</td><td class='drop'>-16</td></tr>
         <tr><td>80</td><td>64</td><td>Sumireko Usami</td><td class='drop'>-16</td></tr>
-        <tr><td>81</td><td>93</td><td>Saki Kurokoma</td><td class='jump'>12</td></tr>
+        <tr><td>81</td><td>93</td><td>Saki Kurokoma</td><td class='jump'>+12</td></tr>
         <tr><td>82</td><td>72</td><td>Yuugi Hoshiguma</td><td class='drop'>-10</td></tr>
         <tr><td>83</td><td>74</td><td>Rinnosuke Morichika</td><td class='drop'>-9</td></tr>
         <tr><td>84</td><td>80</td><td>Hatate Himekaidou</td><td class='drop'>-4</td></tr>
         <tr><td>85</td><td>73</td><td>Soga no Tojiko</td><td class='drop'>-12</td></tr>
-        <tr><td>86</td><td>87</td><td>Hieda no Akyuu</td><td class='jump'>1</td></tr>
+        <tr><td>86</td><td>87</td><td>Hieda no Akyuu</td><td class='jump'>+1</td></tr>
         <tr><td>87</td><td>81</td><td>Wriggle Nightbug</td><td class='drop'>-6</td></tr>
         <tr><td>88</td><td>76</td><td>Komachi Onozuka</td><td class='drop'>-12</td></tr>
         <tr><td>89</td><td>83</td><td>Lunasa Prismriver</td><td class='drop'>-6</td></tr>
-        <tr><td>90</td><td>96</td><td>Lily White</td><td class='jump'>6</td></tr>
+        <tr><td>90</td><td>96</td><td>Lily White</td><td class='jump'>+6</td></tr>
         <tr><td>91</td><td>82</td><td>Shinmyoumaru Sukuna</td><td class='drop'>-9</td></tr>
-        <tr><td>92</td><td>99</td><td>Miyoi Okunoda</td><td class='jump'>7</td></tr>
-        <tr><td>93</td><td>95</td><td>Aunn Komano</td><td class='jump'>2</td></tr>
+        <tr><td>92</td><td>99</td><td>Miyoi Okunoda</td><td class='jump'>+7</td></tr>
+        <tr><td>93</td><td>95</td><td>Aunn Komano</td><td class='jump'>+2</td></tr>
         <tr><td>94</td><td>90</td><td>Shinki</td><td class='drop'>-4</td></tr>
         <tr><td>95</td><td>89</td><td>Shizuha Aki</td><td class='drop'>-6</td></tr>
         <tr><td>96</td><td>85</td><td>Medicine Melancholy</td><td class='drop'>-11</td></tr>
@@ -116,14 +117,14 @@
         <tr><td>99</td><td>88</td><td>Raiko Horikawa</td><td class='drop'>-11</td></tr>
         <tr><td>100</td><td>92</td><td>Minoriko Aki</td><td class='drop'>-8</td></tr>
         <tr><td>101</td><td>84</td><td>Momoyo Himemushi</td><td class='drop'>-17</td></tr>
-        <tr><td>102</td><td>117</td><td>Seiran</td><td class='jump'>15</td></tr>
+        <tr><td>102</td><td>117</td><td>Seiran</td><td class='jump'>+15</td></tr>
         <tr><td>102</td><td>100</td><td>Yumemi Okazaki</td><td class='drop'>-2</td></tr>
         <tr><td>104</td><td>98</td><td>Shou Toramaru</td><td class='drop'>-6</td></tr>
         <tr><td>105</td><td>97</td><td>Wakasagihime</td><td class='drop'>-8</td></tr>
         <tr><td>106</td><td>101</td><td>Letty Whiterock</td><td class='drop'>-5</td></tr>
         <tr><td>107</td><td>94</td><td>Mayumi Joutouguu</td><td class='drop'>-13</td></tr>
-        <tr><td>108</td><td>110</td><td>Star Sapphire</td><td class='jump'>2</td></tr>
-        <tr><td>109</td><td>143</td><td>Sannyo Komakusa</td><td class='jump'>34</td></tr>
+        <tr><td>108</td><td>110</td><td>Star Sapphire</td><td class='jump'>+2</td></tr>
+        <tr><td>109</td><td>143</td><td>Sannyo Komakusa</td><td class='jump'>+34</td></tr>
         <tr><td>110</td><td>105</td><td>Kyouko Kasodani</td><td class='drop'>-5</td></tr>
         <tr><td>111</td><td>111</td><td>Luna Child</td><td class='stable'>0</td></tr>
         <tr><td>112</td><td>102</td><td>Yamame Kurodani</td><td class='drop'>-10</td></tr>
@@ -150,7 +151,7 @@
         <tr><td>133</td><td>132</td><td>Yumeko</td><td class='drop'>-1</td></tr>
         <tr><td>134</td><td>128</td><td>Mugetsu</td><td class='drop'>-6</td></tr>
         <tr><td>135</td><td>130</td><td>Yatsuhashi Tsukumo</td><td class='drop'>-5</td></tr>
-        <tr><td>136</td><td>137</td><td>Mizuchi Miyadeguchi</td><td class='jump'>1</td></tr>
+        <tr><td>136</td><td>137</td><td>Mizuchi Miyadeguchi</td><td class='jump'>+1</td></tr>
         <tr><td>137</td><td>131</td><td>Kisume</td><td class='drop'>-6</td></tr>
         <tr><td>137</td><td>133</td><td>Anxious Moustached Villager</td><td class='drop'>-4</td></tr>
         <tr><td>139</td><td>122</td><td>Takane Yamashiro</td><td class='drop'>-17</td></tr>
@@ -159,12 +160,52 @@
         <tr><td>142</td><td>139</td><td>Chiyuri Kitashirakawa</td><td class='drop'>-3</td></tr>
         <tr><td>143</td><td>134</td><td>Eika Ebisu</td><td class='drop'>-9</td></tr>
         <tr><td>143</td><td>136</td><td>Youki Konpaku</td><td class='drop'>-7</td></tr>
-        <tr><td>145</td><td>148</td><td>Elis</td><td class='jump'>3</td></tr>
-        <tr><td>146</td><td>150</td><td>Ellen</td><td class='jump'>4</td></tr>
+        <tr><td>145</td><td>148</td><td>Elis</td><td class='jump'>+3</td></tr>
+        <tr><td>146</td><td>150</td><td>Ellen</td><td class='jump'>+4</td></tr>
         <tr><td>147</td><td>140</td><td>Urumi Ushizaki</td><td class='drop'>-7</td></tr>
         <tr><td>148</td><td>141</td><td>Nemuno Sakata</td><td class='drop'>-7</td></tr>
         <tr><td>149</td><td>141</td><td>Kotohime</td><td class='drop'>-8</td></tr>
         <tr><td>150</td><td>144</td><td>Konngara</td><td class='drop'>-6</td></tr>
+    </tbody>
+</table></div>
+<p class='center'><strong><a href='#top'>Back to Top</a></strong></p>
+<h2 id='cpg'>Character vote per maingame</h2>
+<h3 id='ctotals'>Totals per maingame</h3>
+<div class='overflow'><table class='poll table sortable noborders'>
+        <colgroup>
+            <col class='col2game'>
+            <col class='col3'>
+            <col class='col4'>
+            <col class='col5'>
+            <col class='col6per'>
+            <col class='col7per'>
+        </colgroup>
+        <thead>
+            <tr>
+                <th class='sorttable_numeric'>Rank</th>
+                <th>Name</th>
+                <th class='sorttable_numeric'>Points</th>
+                <th class='sorttable_numeric'>Change from 2022</th>
+                <th class='sorttable_numeric'>No. 1 Votes</th>
+                <th class='sorttable_numeric'>Comments</th>
+            </tr>
+        </thead>
+    <tbody>
+        <tr><td>1</td><td>EoSD</td><td>65,546</td><td class='drop'>-2,127</td><td>11,598</td><td>14,460</td></tr>
+        <tr><td>2</td><td>PCB</td><td>43,071</td><td class='jump'>+28</td><td>7,055</td><td>8,608</td></tr>
+        <tr><td>3</td><td>Reimu &amp; Marisa</td><td>34,109</td><td class='jump'>+1,852</td><td>6,560</td><td>7,078</td></tr></tr>
+        <tr><td>4</td><td>SA</td><td>33,409</td><td class='jump'>+2,158</td><td>5,289</td><td>6,781</td></tr>
+        <tr><td>5</td><td>IN</td><td>25,469</td><td class='drop'>-366</td><td>3,483</td><td>4,911</td></tr></tr>
+        <tr><td>6</td><td>MoF</td><td>22,760</td><td class='drop'>-1,253</td><td>3,193</td><td>4,517</td></tr>
+        <tr><td>7</td><td>PoFV</td><td>16,028</td><td class='drop'>-1,342</td><td>2,137</td><td>2,937</td></tr>
+        <tr><td>8</td><td>LoLK</td><td>13,521</td><td class='drop'>-1,451</td><td>1,429</td><td>2,722</td></tr>
+        <tr><td>9</td><td>UFO</td><td>13,084</td><td class='drop'>-144</td><td>1,675</td><td>2,464</td></tr>
+        <tr><td>10</td><td>TD</td><td>11,452</td><td class='drop'>-1,181</td><td>1,411</td><td>2,280</td></tr>
+        <tr><td>11</td><td>DDC</td><td>8,972</td><td class='drop'>-674</td><td>1,112</td><td>1,816</td></tr>
+        <tr><td>12</td><td>UM</td><td>7,285</td><td class='drop'>-560</td><td>727</td><td>1,711</td></tr>
+        <tr><td>13</td><td>WBaWC</td><td>7,238</td><td class='jump'>+55</td><td>626</td><td>1,536</td></tr>
+        <tr><td>14</td><td>UDoALG</td><td>7,237</td><td class='stable'>0</td><td>341</td><td>1,868</td></tr>
+        <tr><td>15</td><td>HSiFS</td><td>5,178</td><td class='drop'>-382</td><td>469</td><td>1,065</td></tr>
     </tbody>
 </table></div>
 <footer><strong><a href='#top'>Back to Top</a></strong></footer>
