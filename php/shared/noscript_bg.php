80ad5b1843f3a9e664ea838c7c78193076c74cab
Maribel Hearn <maribelhearn@disroot.org>
2023-10-27 16:53:37 +0200

    [All] noscript_bg: check for empty params

--- php/shared/noscript_bg.php
+++ php/shared/noscript_bg.php
@@ -3,7 +3,8 @@ header('Content-type: text/css');
 $main = array('index', 'about', 'credits', 'privacy', 'error');
 $other = array('thvote', 'tiers', 'slots');
 $personal = array('history', 'c67');
-$page = $_GET['page'];
+$page = !empty($_GET['page']) ? $_GET['page'] : '';
+$pos = !empty($_GET['pos']) ? $_GET['pos'] : 0;
 if (in_array($page, $main)) {
     $directory = 'main';
 } else if (in_array($page, $other)) {
@@ -13,5 +14,5 @@ if (in_array($page, $main)) {
 } else {
     $directory = 'games';
 }
-echo 'body{background:url("../' . $directory . '/' . $page . '/' . $page . '.jpg") ' . $_GET['pos'] . ' no-repeat fixed;background-size:cover}';
+echo 'body{background:url("../' . $directory . '/' . $page . '/' . $page . '.jpg") ' . $pos . ' no-repeat fixed;background-size:cover}';
 ?>
