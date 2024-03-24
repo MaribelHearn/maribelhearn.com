3fdfe67543fcaa6ccdeb6041f3547781ea98648a
Maribel Hearn <maribelhearn@disroot.org>
2024-03-24 21:40:46 +0100

    [Admin] Update flags_gen

--- php/shared/flags_gen.php
+++ php/shared/flags_gen.php
@@ -10,12 +10,12 @@ $flags = array('Afghanistan', 'Aland-Islands', 'Albania', 'Algeria', 'American-S
 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran', 'Iraq', 'Ireland', 'Isle-of-Man', 'Israel', 'Italy', 'Ivory-Coast', 'Jamaica', 'Jan-Mayen', 'Japan', 'Jarvis-Island', 'Jersey', 'Jordan', 'Kazakhstan', 'Kenya',
 'Kiribati', 'South-Korea', 'Kosovo', 'Kuwait', 'Kyrgyzstan', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macao', 'North-Macedonia', 'Madagascar', 'Malawi',
 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Marshall-Islands', 'Martinique', 'Mauritania', 'Mauritius', 'Mayotte', 'Mexico', 'Micronesia', 'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 'Montserrat', 'Morocco',
-'Mozambique', 'Myanmar', 'Namibia', 'Nauru', 'Nepal', 'Netherlands', 'New-Caledonia', 'New-Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Niue', 'Norfolk-Island', 'North-Korea', 'Northern-Mariana-Islands', 'Norway',
+'Mozambique', 'Myanmar', 'Namibia', 'Nauru', 'Nepal', 'The Netherlands', 'New-Caledonia', 'New-Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Niue', 'Norfolk-Island', 'North-Korea', 'Northern-Mariana-Islands', 'Norway',
 'Oman', 'Pakistan', 'Palau', 'Palestine', 'Panama', 'Papua-New-Guinea', 'Paraguay', 'Peru', 'Philippines', 'Pitcairn', 'Poland', 'Portugal', 'Puerto-Rico', 'Qatar', 'Reunion', 'Romania', 'Russia',
 'Rwanda', 'Saint-Barthelemy', 'Saint-Helena-Dependencies', 'Saint-Helena', 'Saint-Kitts-and-Nevis', 'Saint-Lucia', 'Saint-Martin', 'Saint-Pierre-and-Miquelon', 'Saint-Vincent-and-the-Grenadines', 'Samoa',
 'San-Marino', 'Sao-Tome-and-Principe', 'Saudi-Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra-Leone', 'Singapore', 'Slovakia', 'Slovenia', 'Solomon-Islands', 'Somalia', 'South-Africa', 'South-Georgia', 'Spain',
 'Sri-Lanka', 'Sudan', 'Suriname', 'Svalbard', 'Swaziland', 'Sweden', 'Switzerland', 'Syria', 'Taiwan', 'Tajikistan', 'Tanzania', 'Thailand', 'Timor-Leste', 'Togo', 'Tokelau', 'Tonga', 'Trinidad-and-Tobago',
-'Tunisia', 'Turkey', 'Turkmenistan', 'Turks-and-Caicos-Islands', 'Tuvalu', 'Uganda', 'Ukraine', 'United-Arab-Emirates', 'United-Kingdom', 'United-States', 'Uruguay', 'Uzbekistan', 'Vanuatu', 'Vatican-City',
+'Tunisia', 'Türkiye', 'Turkmenistan', 'Turks-and-Caicos-Islands', 'Tuvalu', 'Uganda', 'Ukraine', 'United-Arab-Emirates', 'United-Kingdom', 'United-States', 'Uruguay', 'Uzbekistan', 'Vanuatu', 'Vatican-City',
 'Venezuela', 'Vietnam', 'Virgin-Islands', 'Wallis-and-Futuna', 'Western-Sahara', 'Yemen', 'Zambia', 'Zimbabwe');
 for ($i = 0; $i < count($flags); $i++) {
     $x = ($i % $row_size) * $offset;
