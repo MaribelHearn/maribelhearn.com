5deb23143c33b54e029c31a91dc91a8b3cb7de8d
Maribel Hearn <maribelhearn@disroot.org>
2024-05-31 11:18:11 +0200

    [LNN] Add missing ordering to game query

--- js/lnn.js
+++ js/lnn.js
@@ -70,7 +70,7 @@ function getLNNs(game) {
     }
 
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', `${API_BASE}/api/v1/replay/?type=LNN&game=${game}`);
+    xhr.open('GET', `${API_BASE}/api/v1/replay/?type=LNN&ordering=shot&game=${game}`);
     xhr.onreadystatechange = function () {
         if (this.readyState === 4) {
             if (this.status === 200) {
