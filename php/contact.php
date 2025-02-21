75804311bef01db8170769385b8fd01f13ce6d88
Maribel Hearn <maribelhearn@disroot.org>
2025-02-21 10:14:47 +0100

    [Contact] Update Contact page

--- php/contact.php
+++ php/contact.php
@@ -8,4 +8,10 @@
         <li>Discord: <span class='icon discord_icon'></span> maribelhearn</li>
         <li>Bluesky: <a href='https://bsky.app/profile/maribelhearn42.bsky.social' target='_blank'><span class='icon bluesky_icon'></span> maribelhearn42</a></li>
     </ul>
+    <p class='wide-top'>If you want to buy me a coffee, you can do so through my Ko-fi page:</p>
+    <p class='donation wide-top'>
+        <a href='https://ko-fi.com/maribelhearn42'>
+            <span class='icon kofi_icon'></span> Support me
+        </a>
+    </p>
 </div>
